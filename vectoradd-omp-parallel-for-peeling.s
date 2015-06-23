7392e620414b29d974e53f608a8ed589debe6d29
rogerio.rag <rogerio.rag@gmail.com>
2015-06-23 09:47:26 -0300

    vectoradd-omp-parallel-for-peeling.s

--- vectoradd-omp-parallel-for-peeling.s
+++ vectoradd-omp-parallel-for-peeling.s
@@ -3,29 +3,29 @@
 	.bss
 	.align 32
 	.type	h_a, @object
-	.size	h_a, 4194304
+	.size	h_a, 64
 h_a:
-	.zero	4194304
+	.zero	64
 	.globl	h_b
 	.align 32
 	.type	h_b, @object
-	.size	h_b, 4194304
+	.size	h_b, 64
 h_b:
-	.zero	4194304
+	.zero	64
 	.globl	h_c
 	.align 32
 	.type	h_c, @object
-	.size	h_c, 4194304
+	.size	h_c, 64
 h_c:
-	.zero	4194304
+	.zero	64
 	.section	.rodata
 .LC0:
-	.string	"Inicializando os arrays.\n"
+	.string	"init_array().\n"
 	.text
 	.globl	_Z10init_arrayv
 	.type	_Z10init_arrayv, @function
 _Z10init_arrayv:
-.LFB2:
+.LFB0:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -35,7 +35,7 @@ _Z10init_arrayv:
 	subq	$16, %rsp
 	movq	stdout(%rip), %rax
 	movq	%rax, %rcx
-	movl	$25, %edx
+	movl	$14, %edx
 	movl	$1, %esi
 	movl	$.LC0, %edi
 	call	fwrite
@@ -52,7 +52,7 @@ _Z10init_arrayv:
 	movl	%eax, h_b(,%rdx,4)
 	addl	$1, -4(%rbp)
 .L2:
-	cmpl	$1048575, -4(%rbp)
+	cmpl	$15, -4(%rbp)
 	setle	%al
 	testb	%al, %al
 	jne	.L3
@@ -60,16 +60,18 @@ _Z10init_arrayv:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE2:
+.LFE0:
 	.size	_Z10init_arrayv, .-_Z10init_arrayv
 	.section	.rodata
 .LC2:
+	.string	"print_array().\n"
+.LC3:
 	.string	"h_c[%07d]: %f\n"
 	.text
 	.globl	_Z11print_arrayv
 	.type	_Z11print_arrayv, @function
 _Z11print_arrayv:
-.LFB3:
+.LFB1:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -77,6 +79,12 @@ _Z11print_arrayv:
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
 	subq	$16, %rsp
+	movq	stdout(%rip), %rax
+	movq	%rax, %rcx
+	movl	$15, %edx
+	movl	$1, %esi
+	movl	$.LC2, %edi
+	call	fwrite
 	movl	$0, -4(%rbp)
 	jmp	.L5
 .L6:
@@ -87,13 +95,13 @@ _Z11print_arrayv:
 	cvtps2pd	%xmm0, %xmm0
 	movq	stdout(%rip), %rax
 	movl	-4(%rbp), %edx
-	movl	$.LC2, %esi
+	movl	$.LC3, %esi
 	movq	%rax, %rdi
 	movl	$1, %eax
 	call	fprintf
 	addl	$1, -4(%rbp)
 .L5:
-	cmpl	$1048575, -4(%rbp)
+	cmpl	$15, -4(%rbp)
 	setle	%al
 	testb	%al, %al
 	jne	.L6
@@ -101,18 +109,20 @@ _Z11print_arrayv:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE3:
+.LFE1:
 	.size	_Z11print_arrayv, .-_Z11print_arrayv
 	.section	.rodata
 .LC4:
-	.string	"Verificando o resultado.\n"
+	.string	"check_result().\n"
 .LC6:
+	.string	"Verificando o resultado.\n"
+.LC8:
 	.string	"Resultado Final: (%f, %f)\n"
 	.text
 	.globl	_Z12check_resultv
 	.type	_Z12check_resultv, @function
 _Z12check_resultv:
-.LFB4:
+.LFB2:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -120,38 +130,44 @@ _Z12check_resultv:
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
 	subq	$16, %rsp
-	movl	.LC3(%rip), %eax
-	movl	%eax, -8(%rbp)
+	movq	stdout(%rip), %rax
+	movq	%rax, %rcx
+	movl	$16, %edx
+	movl	$1, %esi
+	movl	$.LC4, %edi
+	call	fwrite
+	movl	.LC5(%rip), %eax
+	movl	%eax, -4(%rbp)
 	movq	stdout(%rip), %rax
 	movq	%rax, %rcx
 	movl	$25, %edx
 	movl	$1, %esi
-	movl	$.LC4, %edi
+	movl	$.LC6, %edi
 	call	fwrite
-	movl	$0, -4(%rbp)
+	movl	$0, -8(%rbp)
 	jmp	.L8
 .L9:
-	movl	-4(%rbp), %eax
+	movl	-8(%rbp), %eax
 	cltq
 	movss	h_c(,%rax,4), %xmm0
-	movss	-8(%rbp), %xmm1
+	movss	-4(%rbp), %xmm1
 	addss	%xmm1, %xmm0
-	movss	%xmm0, -8(%rbp)
-	addl	$1, -4(%rbp)
+	movss	%xmm0, -4(%rbp)
+	addl	$1, -8(%rbp)
 .L8:
-	cmpl	$1048575, -4(%rbp)
+	cmpl	$15, -8(%rbp)
 	setle	%al
 	testb	%al, %al
 	jne	.L9
-	movss	-8(%rbp), %xmm0
-	movss	.LC5(%rip), %xmm1
+	movss	-4(%rbp), %xmm0
+	movss	.LC7(%rip), %xmm1
 	divss	%xmm1, %xmm0
 	unpcklps	%xmm0, %xmm0
 	cvtps2pd	%xmm0, %xmm1
-	movss	-8(%rbp), %xmm0
+	movss	-4(%rbp), %xmm0
 	cvtps2pd	%xmm0, %xmm0
 	movq	stdout(%rip), %rax
-	movl	$.LC6, %esi
+	movl	$.LC8, %esi
 	movq	%rax, %rdi
 	movl	$2, %eax
 	call	fprintf
@@ -159,12 +175,20 @@ _Z12check_resultv:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE4:
+.LFE2:
 	.size	_Z12check_resultv, .-_Z12check_resultv
+	.section	.rodata
+.LC9:
+	.string	"main().\n"
+.LC10:
+	.string	"before parallel region 1.\n"
+.LC11:
+	.string	"before parallel region 2.\n"
+	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB5:
+.LFB3:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -172,7 +196,19 @@ main:
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
 	subq	$16, %rsp
+	movq	stdout(%rip), %rax
+	movq	%rax, %rcx
+	movl	$8, %edx
+	movl	$1, %esi
+	movl	$.LC9, %edi
+	call	fwrite
 	call	_Z10init_arrayv
+	movq	stdout(%rip), %rax
+	movq	%rax, %rcx
+	movl	$26, %edx
+	movl	$1, %esi
+	movl	$.LC10, %edi
+	call	fwrite
 	movl	-4(%rbp), %eax
 	movl	%eax, -16(%rbp)
 	leaq	-16(%rbp), %rax
@@ -186,6 +222,12 @@ main:
 	call	GOMP_parallel_end
 	movl	-16(%rbp), %eax
 	movl	%eax, -4(%rbp)
+	movq	stdout(%rip), %rax
+	movq	%rax, %rcx
+	movl	$26, %edx
+	movl	$1, %esi
+	movl	$.LC11, %edi
+	call	fwrite
 	movl	$0, %edx
 	movl	$0, %esi
 	movl	$main._omp_fn.1, %edi
@@ -199,19 +241,27 @@ main:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE5:
+.LFE3:
 	.size	main, .-main
+	.section	.rodata
+.LC12:
+	.string	"Thread: %d of %d.\n"
+	.text
 	.type	main._omp_fn.0, @function
 main._omp_fn.0:
-.LFB6:
+.LFB4:
 	.cfi_startproc
+	.cfi_personality 0x3,__gxx_personality_v0
+	.cfi_lsda 0x3,.LLSDA4
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movq	%rdi, -8(%rbp)
+	pushq	%rbx
+	subq	$24, %rsp
+	.cfi_offset 3, -24
+	movq	%rdi, -24(%rbp)
 	call	GOMP_single_start
 	cmpb	$1, %al
 	je	.L13
@@ -219,64 +269,90 @@ main._omp_fn.0:
 	call	GOMP_barrier
 	jmp	.L16
 .L13:
-	movq	-8(%rbp), %rax
+	movq	-24(%rbp), %rax
 	movl	$0, (%rax)
 .L15:
-	movq	-8(%rbp), %rax
+	movq	-24(%rbp), %rax
 	movl	(%rax), %eax
-	cmpl	$524287, %eax
+	cmpl	$7, %eax
 	setle	%al
 	testb	%al, %al
 	je	.L14
-	movq	-8(%rbp), %rax
+	call	omp_get_num_threads
+	movl	%eax, %ebx
+	call	omp_get_thread_num
+	movl	%eax, %edx
+	movq	stdout(%rip), %rax
+	movl	%ebx, %ecx
+	movl	$.LC12, %esi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf
+	movq	-24(%rbp), %rax
 	movl	(%rax), %eax
 	cltq
 	movss	h_a(,%rax,4), %xmm1
-	movq	-8(%rbp), %rax
+	movq	-24(%rbp), %rax
 	movl	(%rax), %eax
 	cltq
 	movss	h_b(,%rax,4), %xmm0
 	addss	%xmm1, %xmm0
-	movq	-8(%rbp), %rax
+	movq	-24(%rbp), %rax
 	movl	(%rax), %eax
 	cltq
 	movss	%xmm0, h_c(,%rax,4)
-	movq	-8(%rbp), %rax
+	movq	-24(%rbp), %rax
 	movl	(%rax), %eax
 	leal	1(%rax), %edx
-	movq	-8(%rbp), %rax
+	movq	-24(%rbp), %rax
 	movl	%edx, (%rax)
 	jmp	.L15
 .L16:
-	leave
+	addq	$24, %rsp
+	popq	%rbx
+	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE6:
+.LFE4:
+	.globl	__gxx_personality_v0
+	.section	.gcc_except_table,"a",@progbits
+.LLSDA4:
+	.byte	0xff
+	.byte	0xff
+	.byte	0x1
+	.uleb128 .LLSDACSE4-.LLSDACSB4
+.LLSDACSB4:
+.LLSDACSE4:
+	.text
 	.size	main._omp_fn.0, .-main._omp_fn.0
 	.type	main._omp_fn.1, @function
 main._omp_fn.1:
-.LFB7:
+.LFB5:
 	.cfi_startproc
+	.cfi_personality 0x3,__gxx_personality_v0
+	.cfi_lsda 0x3,.LLSDA5
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	pushq	%r12
 	pushq	%rbx
-	subq	$40, %rsp
-	.cfi_offset 3, -24
+	subq	$32, %rsp
+	.cfi_offset 12, -24
+	.cfi_offset 3, -32
 	movq	%rdi, -40(%rbp)
 	call	omp_get_num_threads
 	movl	%eax, %ebx
 	call	omp_get_thread_num
 	movl	%eax, %esi
-	movl	$524288, %eax
+	movl	$8, %eax
 	movl	%eax, %edx
 	sarl	$31, %edx
 	idivl	%ebx
 	movl	%eax, %ecx
-	movl	$524288, %eax
+	movl	$8, %eax
 	movl	%eax, %edx
 	sarl	$31, %edx
 	idivl	%ebx
@@ -290,10 +366,20 @@ main._omp_fn.1:
 	leal	(%rax,%rcx), %edx
 	cmpl	%edx, %eax
 	jge	.L17
-	addl	$524288, %eax
+	addl	$8, %eax
 	movl	%eax, -20(%rbp)
-	addl	$524288, %edx
+	leal	8(%rdx), %r12d
 .L20:
+	call	omp_get_num_threads
+	movl	%eax, %ebx
+	call	omp_get_thread_num
+	movl	%eax, %edx
+	movq	stdout(%rip), %rax
+	movl	%ebx, %ecx
+	movl	$.LC12, %esi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf
 	movl	-20(%rbp), %eax
 	cltq
 	movss	h_a(,%rax,4), %xmm1
@@ -305,7 +391,7 @@ main._omp_fn.1:
 	cltq
 	movss	%xmm0, h_c(,%rax,4)
 	addl	$1, -20(%rbp)
-	cmpl	%edx, -20(%rbp)
+	cmpl	%r12d, -20(%rbp)
 	jl	.L20
 	jmp	.L17
 .L18:
@@ -313,23 +399,33 @@ main._omp_fn.1:
 	addl	$1, %ecx
 	jmp	.L21
 .L17:
-	addq	$40, %rsp
+	addq	$32, %rsp
 	popq	%rbx
+	popq	%r12
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE7:
+.LFE5:
+	.section	.gcc_except_table
+.LLSDA5:
+	.byte	0xff
+	.byte	0xff
+	.byte	0x1
+	.uleb128 .LLSDACSE5-.LLSDACSB5
+.LLSDACSB5:
+.LLSDACSE5:
+	.text
 	.size	main._omp_fn.1, .-main._omp_fn.1
 	.section	.rodata
 	.align 4
 .LC1:
 	.long	1056964608
 	.align 4
-.LC3:
+.LC5:
 	.long	0
 	.align 4
-.LC5:
-	.long	1233125376
-	.ident	"GCC: (Debian 4.7.4-3) 4.7.4"
+.LC7:
+	.long	1098907648
+	.ident	"GCC: (Ubuntu/Linaro 4.7.2-2ubuntu1) 4.7.2"
 	.section	.note.GNU-stack,"",@progbits
