09666be9892a7278a97454e24c5758d405243f9c
rogerio.rag <rogerio.rag@gmail.com>
2015-06-23 04:46:11 -0300

    Updating libhookomp to show papi counters.

--- hookomp.c
+++ hookomp.c
@@ -10,64 +10,52 @@
 long_long values[NUM_EVENTS];
 int Events[NUM_EVENTS]={PAPI_TOT_INS, PAPI_TOT_CYC};
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
 #endif
-		void foo(void);
-		// GOMP_loop_runtime_start@@GOMP_1.0
-		
-		void GOMP_parallel_start (void (*fn)(void *), void *data, unsigned num_threads);
-
-    void GOMP_parallel_end (void);
-		
-		
+	void foo(void);
+	// GOMP_loop_runtime_start@@GOMP_1.0
+	void GOMP_parallel_start (void (*fn)(void *), void *data, unsigned num_threads);
+	void GOMP_parallel_end (void);
 #ifdef __cplusplus
 }
 #endif
 
-
- 
+/* Test function. */
 void foo(void) {
-    puts("Hello, I'm a shared library");
-} 
+	puts("Hello, I'm a shared library");
+}
 
 /* Function to intercept GOMP_parallel_start */
 void GOMP_parallel_start (void (*fn)(void *), void *data, unsigned num_threads){
-	 printf("[hookomp] GOMP_parallel_start.\n");
-	 	 
-	 typedef void (*func_t)(void (*fn)(void *), void *, unsigned);
-	 
-	 func_t lib_GOMP_parallel_start = (func_t) dlsym(RTLD_NEXT, "GOMP_parallel_start");
-   printf("[GOMP_1.0] GOMP_parallel_start@@GOMP_1.0.\n");
-   
-	 /* Start the counters */
-	 PAPI_start_counters((int*)Events, NUM_EVENTS);
-	 
-	 return lib_GOMP_parallel_start(fn, data, num_threads);
-	 
+	printf("[hookomp] GOMP_parallel_start.\n");
+
+	typedef void (*func_t)(void (*fn)(void *), void *, unsigned);
+
+	func_t lib_GOMP_parallel_start = (func_t) dlsym(RTLD_NEXT, "GOMP_parallel_start");
+	printf("[GOMP_1.0] GOMP_parallel_start@@GOMP_1.0.\n");
+
+	/* Start the counters */
+	PAPI_start_counters((int*)Events, NUM_EVENTS);
+
+	return lib_GOMP_parallel_start(fn, data, num_threads); 
 }
 
 /* Function to intercept GOMP_parallel_end */
 void GOMP_parallel_end (void){
 	printf("[hookomp] GOMP_parallel_end.\n");
-	 
+
 	/* Stop counters and store results in values */
 	int retval = PAPI_stop_counters(values,NUM_EVENTS);
-	
-	if(retval)
-		printf("Total insts: %lld Total Cycles: %lld\n", (long long int) Events[0], (long long int) Events[1]);	 
-	 
+
+	if(retval != PAPI_OK )
+           printf("Erro on PAPI execution.");
+
+	printf("Total insts: %lld Total Cycles: %lld\n", (long long int) Events[0], (long long int) Events[1]);	 
+
 	typedef void (*func_t)(void);
-	 
+
 	func_t lib_GOMP_parallel_end = (func_t) dlsym(RTLD_NEXT, "GOMP_parallel_end");
-  printf("[GOMP_1.0] GOMP_parallel_end@@GOMP_1.0.\n");
-  return lib_GOMP_parallel_end();	 
+	printf("[GOMP_1.0] GOMP_parallel_end@@GOMP_1.0.\n");
+	return lib_GOMP_parallel_end();	 
 }
-
-
-
-	
-	
-	
-
-
