6e03723cc93dd8a0a565afc59aeb1cb415f95afb
rogerio.rag <rogerio.rag@gmail.com>
2015-06-24 15:28:14 +0200

    Updating vectoradd-omp-loops papi and openmp sample.

--- vectoradd-omp-loops.c
+++ vectoradd-omp-loops.c
@@ -5,7 +5,6 @@
 #include <omp.h>
 #else
 #define omp_get_thread_num() 0
-#define omp_get_thread_num 0
 #define omp_get_num_threads() 1
 #define omp_get_num_procs() (system("cat /proc/cpuinfo | grep 'processor' | wc -l"))
 #endif
