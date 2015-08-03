28e223c4b990ba7c397e035f735e8a800384d6ba
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-08-03 14:49:36 +0200

    Updating hook omp.

--- hookomp.c
+++ hookomp.c
@@ -88,6 +88,7 @@ void GOMP_parallel_start (void (*fn)(void *), void *data, unsigned num_threads){
 
 	printf("[hookomp]   Call by TablePointerFunctions.\n");
 	TablePointerFunctions[0](data);
+	TablePointerFunctions[1](data);
 	
 	typedef void (*func_t)(void (*fn)(void *), void *, unsigned);
 	func_t lib_GOMP_parallel_start = (func_t) dlsym(RTLD_NEXT, "GOMP_parallel_start");
