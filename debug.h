351a9ff3481f444942231df8c12d8ecc18c98cfa
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-09-30 23:58:35 +0200

    Updating debug macros.

--- debug.h
+++ debug.h
@@ -6,7 +6,7 @@
 // #define VERBOSE 1
 
 #if defined(DEBUG) && DEBUG > 0
- #define TRACE(fmt, args...)	do{ fprintf(stderr, "DEBUG: [%s]:%d:%s(): " fmt, \
+ #define TRACE(fmt, args...)	do{ fprintf(stderr, "DEBUG: [%10s:%07d] in %s(): " fmt, \
     __FILE__, __LINE__, __func__, ##args); } while(0)
 #else
  #define TRACE(fmt, args...) do{ } while (0)
@@ -15,7 +15,7 @@
 // #define VERBOSE 1
 // Use make OPTIONS=-DVERBOSE
 #if defined(VERBOSE) && VERBOSE > 0
-#define PRINT_FUNC_NAME fprintf(stderr, "[%s] Calling [%s]\n", __FILE__, __FUNCTION__)
+#define PRINT_FUNC_NAME fprintf(stderr, "TRACE: [%10s:%07d] Calling [%s()]\n", __FILE__,__LINE__, __FUNCTION__)
 #else
 #define PRINT_FUNC_NAME (void) 0
 #endif
