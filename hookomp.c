b99778c32ff9863be07b1a667a42c16d2227a48e
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-10-07 16:52:11 +0200

    Refactoring to create a HOOKOMP_initialization function.

--- hookomp.c
+++ hookomp.c
@@ -45,6 +45,73 @@ void release_all_team_threads(void){
 }
 
 /* ------------------------------------------------------------- */
+/* Generic function to get next chunk. */
+bool HOOKOMP_generic_next (long *istart, long *iend, bool (*fn_next_chunk) (long *, long *)){
+	PRINT_FUNC_NAME;
+	bool result = false;
+	TRACE("[hookomp]: Thread [%lu] is calling %s.\n", (long int) pthread_self(), __FUNCTION__);
+
+	/* Registry the thread which will be execute alone. down semaphore. */
+	sem_wait(&mutex_registry_thread_in_func_next);
+
+	if(thread_executing_function_next == -1){
+		thread_executing_function_next = pthread_self();
+		TRACE("[hookomp]: Thread [%lu] is entering in controled execution.\n", (long int) thread_executing_function_next);
+	}
+	/* up semaphore. */
+  	sem_post(&mutex_registry_thread_in_func_next);
+
+	int total_of_iterations = 0;
+
+	/* Verify if the thread is the thread executing. */
+	if(thread_executing_function_next == (long int) pthread_self()){
+		total_of_iterations = (loop_iterations_end - loop_iterations_start);
+
+		if(executed_loop_iterations < (total_of_iterations / percentual_of_code)){
+			TRACE("[hookomp]: [Before]-> GOMP_loop_runtime_next -- Tid[%lu] istart: %ld iend: %ld.\n", thread_executing_function_next, *istart, *iend);
+			result = fn_next_chunk(istart, iend);
+			TRACE("[hookomp]: [After]-> GOMP_loop_runtime_next -- Tid[%lu] istart: %ld iend: %ld.\n", thread_executing_function_next, *istart, *iend);
+			/* Update the number of iterations executed by this thread. */
+			TRACE("[hookomp]: [Before]-> GOMP_loop_runtime_next -- Tid[%lu] executed iterations: %ld.\n", thread_executing_function_next, executed_loop_iterations);
+			executed_loop_iterations += (*iend - *istart);
+			TRACE("[hookomp]: [After]-> GOMP_loop_runtime_next -- Tid[%lu] executed iterations: %ld.\n", thread_executing_function_next, executed_loop_iterations);
+
+			/* PAPI Start the counters. */
+			if(!started_measuring){
+				if(RM_start_counters()){
+   					TRACE("[hookomp] GOMP_single_start: PAPI Counters Started.\n");
+   				}
+   				else {
+   					TRACE("Error calling RM_start_counters from GOMP_single_start.\n");
+   				}
+   				started_measuring = true;
+   			}
+		}
+		else{
+			TRACE("[hookomp]: GOMP_loop_runtime_next -- Tid[%lu] executed %ld iterations of %ld.\n", thread_executing_function_next, executed_loop_iterations, (loop_iterations_end - loop_iterations_start));
+		}
+	}
+	else{
+		/* If it is executing in a section to measurements, the threads will be blocked. */		
+		if (is_executed_measures_section){
+			/* Other team threads will be blocked. */
+			TRACE("[hookomp]: Thread [%lu] will be blocked.\n", (long int) pthread_self());
+			sem_wait(&sem_blocks_other_team_threads);	
+		}
+		
+		// result = lib_GOMP_loop_runtime_next(istart, iend);
+		/* if decided by offloading, no more work to do, so return false. */
+		if(!decided_by_offloading){
+			result = fn_next_chunk(istart, iend);	
+		}
+		else{
+			result = false;
+		}
+	}
+	return result;
+}
+
+/* ------------------------------------------------------------- */
 /* barrier.c                                                     */
 /* ------------------------------------------------------------- */
 void GOMP_barrier (void) {
@@ -55,27 +122,26 @@ void GOMP_barrier (void) {
 	TRACE("[hookomp]: Thread [%lu] is executing barrier, single region was executed by [%lu].\n", (unsigned long int) pthread_self(), (unsigned long int) executing_a_single_region);
 
 	/* Matching the thread executing barrier with thread that entered in single region. */
-	// if(executing_a_single_region == omp_get_thread_num()){
-	if(executing_a_single_region == (long int) pthread_self()){
-		TRACE("[hookomp]: Thread [%lu] is exiting of single region.\n", (long int) executing_a_single_region);
+	// if(executing_a_single_region == (long int) pthread_self()){
+	// 	TRACE("[hookomp]: Thread [%lu] is exiting of single region.\n", (long int) executing_a_single_region);
 
-		if(!RM_stop_counters()){
-			TRACE("Error GOMP_barrier: RM_stop_counters.\n");
-		}
-		else{
-			// Verificar o que a GOMP_barrier faz para ver onde a chamada aos 
-			// contadores tem que ser feita.
+	// 	if(!RM_stop_counters()){
+	// 		TRACE("Error GOMP_barrier: RM_stop_counters.\n");
+	// 	}
+	// 	else{
+	// 		// Verificar o que a GOMP_barrier faz para ver onde a chamada aos 
+	// 		// contadores tem que ser feita.
     
-    		// A decisão de migrar é aqui.
-			// double oi = RM_get_operational_intensity();
-			// TRACE("Operational intensity: %10.2f\n", oi);
+ 	//    	// A decisão de migrar é aqui.
+	// 		// double oi = RM_get_operational_intensity();
+	// 		// TRACE("Operational intensity: %10.2f\n", oi);
 
-			// int better_device = RM_get_better_device_to_execution();
-			// TRACE("Execution is better on device [%d].\n", better_device);
-		}
+	// 		// int better_device = RM_get_better_device_to_execution();
+	// 		// TRACE("Execution is better on device [%d].\n", better_device);
+	// 	}
 		
-		executing_a_single_region = -1;
-	}
+	// 	executing_a_single_region = -1;
+	// }
 
 	TRACE("[GOMP_1.0] GOMP_barrier@GOMP_1.0.\n");
 	lib_GOMP_barrier();
@@ -273,72 +339,6 @@ bool GOMP_loop_ordered_runtime_start (long start, long end, long incr,
 	
 	bool result = lib_GOMP_loop_ordered_runtime_start(start, end, incr, istart, iend);
 	
-	return result;
-}
-
-/* ------------------------------------------------------------- */
-bool HOOKOMP_generic_next (long *istart, long *iend, bool (*fn_next_chunk) (long *, long *)){
-	PRINT_FUNC_NAME;
-	bool result = false;
-	TRACE("[hookomp]: Thread [%lu] is calling %s.\n", (long int) pthread_self(), __FUNCTION__);
-
-	/* Registry the thread which will be execute alone. down semaphore. */
-	sem_wait(&mutex_registry_thread_in_func_next);
-
-	if(thread_executing_function_next == -1){
-		thread_executing_function_next = pthread_self();
-		TRACE("[hookomp]: Thread [%lu] is entering in controled execution.\n", (long int) thread_executing_function_next);
-	}
-	/* up semaphore. */
-  	sem_post(&mutex_registry_thread_in_func_next);
-
-	int total_of_iterations = 0;
-
-	/* Verify if the thread is the thread executing. */
-	if(thread_executing_function_next == (long int) pthread_self()){
-		total_of_iterations = (loop_iterations_end - loop_iterations_start);
-
-		if(executed_loop_iterations < (total_of_iterations / percentual_of_code)){
-			TRACE("[hookomp]: [Before]-> GOMP_loop_runtime_next -- Tid[%lu] istart: %ld iend: %ld.\n", thread_executing_function_next, *istart, *iend);
-			result = fn_next_chunk(istart, iend);
-			TRACE("[hookomp]: [After]-> GOMP_loop_runtime_next -- Tid[%lu] istart: %ld iend: %ld.\n", thread_executing_function_next, *istart, *iend);
-			/* Update the number of iterations executed by this thread. */
-			TRACE("[hookomp]: [Before]-> GOMP_loop_runtime_next -- Tid[%lu] executed iterations: %ld.\n", thread_executing_function_next, executed_loop_iterations);
-			executed_loop_iterations += (*iend - *istart);
-			TRACE("[hookomp]: [After]-> GOMP_loop_runtime_next -- Tid[%lu] executed iterations: %ld.\n", thread_executing_function_next, executed_loop_iterations);
-
-			/* PAPI Start the counters. */
-			if(!started_measuring){
-				if(RM_start_counters()){
-   					TRACE("[hookomp] GOMP_single_start: PAPI Counters Started.\n");
-   				}
-   				else {
-   					TRACE("Error calling RM_start_counters from GOMP_single_start.\n");
-   				}
-   				started_measuring = true;
-   			}
-		}
-		else{
-			TRACE("[hookomp]: GOMP_loop_runtime_next -- Tid[%lu] executed %ld iterations of %ld.\n", thread_executing_function_next, executed_loop_iterations, (loop_iterations_end - loop_iterations_start));
-		}
-	}
-	else{
-		/* If it is executing in a section to measurements, the threads will be blocked. */		
-		if (is_executed_measures_section){
-			/* Other team threads will be blocked. */
-			TRACE("[hookomp]: Thread [%lu] will be blocked.\n", (long int) pthread_self());
-			sem_wait(&sem_blocks_other_team_threads);	
-		}
-		
-		// result = lib_GOMP_loop_runtime_next(istart, iend);
-		/* if decided by offloading, no more work to do, so return false. */
-		if(!decided_by_offloading){
-			result = fn_next_chunk(istart, iend);	
-		}
-		else{
-			result = false;
-		}
-	}
 	return result;
 }
 
@@ -601,12 +601,12 @@ void GOMP_parallel_loop_runtime_start (void (*fn) (void *), void *data,
 
 	decided_by_offloading = false;
 
-	lib_GOMP_parallel_loop_runtime_start(fn, data, num_threads, start, end, incr);
-	
 	/* Initialize RM library. */
   	if(!RM_library_init()){
   		TRACE("GOMP_parallel_start: error RM_library_init.\n");
   	}
+	
+	lib_GOMP_parallel_loop_runtime_start(fn, data, num_threads, start, end, incr);
 }
 
 /* ------------------------------------------------------------- */
