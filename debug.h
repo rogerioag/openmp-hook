b5f03966728f0ebbefdef160bb2e610eb1b1c775
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-10-13 14:55:05 +0200

    Return to TRACE macro only.

--- debug.h
+++ debug.h
@@ -6,20 +6,17 @@
 #define VERBOSE 1
 
 #if defined(DEBUG) && DEBUG > 0
- #define PRINT_MSG(type, fmt, args...)	do{ fprintf(stderr, "%s: [%10s:%07d] Thread [%lu] in %s(): " fmt, \
+ #define TRACE(fmt, args...)	do{ fprintf(stderr, "[TRACE]: [%10s:%07d] Thread [%lu] in %s(): " fmt, \
     type, __FILE__, __LINE__, (long int) pthread_self(), __func__, ##args); } while(0)
 
- #define PRT_DEBUG(fmt, args...) PRINT_MSG("DEBUG",fmt,args)
-
- #define TRACE(fmt, args...) PRINT_MSG("TRACE",fmt,args)
 #else
- #define PRINT_MSG(type, fmt, args...) do{ } while (0)
+ #define TRACE(fmt, args...) do{ } while (0)
 #endif
 
 // #define VERBOSE 1
 // Use make OPTIONS=-DVERBOSE
 #if defined(VERBOSE) && VERBOSE > 0
-#define PRINT_FUNC_NAME fprintf(stderr, "TRACE-FUNCNAME: [%10s:%07d] Thread [%lu] is calling [%s()]\n", __FILE__, __LINE__, (long int) pthread_self(), __FUNCTION__)
+#define PRINT_FUNC_NAME fprintf(stderr, "TRACE-FUNC-NAME: [%10s:%07d] Thread [%lu] is calling [%s()]\n", __FILE__, __LINE__, (long int) pthread_self(), __FUNCTION__)
 #else
 #define PRINT_FUNC_NAME (void) 0
 #endif
