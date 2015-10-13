b2d7b280d3ac935817345ae481949a9db8df371d
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-10-13 14:45:27 +0200

    Improving the TRACE/DEBUG macros.

--- debug.h
+++ debug.h
@@ -6,8 +6,8 @@
 #define VERBOSE 1
 
 #if defined(DEBUG) && DEBUG > 0
- #define PRINT_MSG(type, fmt, args...)	do{ fprintf(stderr, "%s: [%10s:%07d] Thread [%lu] in %s(): " type, fmt, \
-    __FILE__, __LINE__, (long int) pthread_self(), __func__, ##args); } while(0)
+ #define PRINT_MSG(type, fmt, args...)	do{ fprintf(stderr, "%s: [%10s:%07d] Thread [%lu] in %s(): " fmt, \
+    type, __FILE__, __LINE__, (long int) pthread_self(), __func__, ##args); } while(0)
 
  #define PRT_DEBUG (fmt, args...) PRINT_MSG("DEBUG",fmt, args...)
  #define TRACE (fmt, args...) PRINT_MSG("TRACE",fmt, args...)
