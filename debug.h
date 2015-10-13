ca047ce46bcaaa08a22266cee8da700474df9945
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-10-13 14:11:52 +0200

    Adding Thread id in DEBUG messages.

--- debug.h
+++ debug.h
@@ -15,9 +15,10 @@
 // #define VERBOSE 1
 // Use make OPTIONS=-DVERBOSE
 #if defined(VERBOSE) && VERBOSE > 0
-#define PRINT_FUNC_NAME fprintf(stderr, "TRACE: [%10s:%07d] Calling [%s()]\n", __FILE__,__LINE__, __FUNCTION__)
+#define PRINT_FUNC_NAME fprintf(stderr, "TRACE: [%10s:%07d] Thread [%lu] is calling [%s()]\n", __FILE__, __LINE__, (long int) pthread_self(), __FUNCTION__)
 #else
 #define PRINT_FUNC_NAME (void) 0
 #endif
 
 #endif /* DEBUG_H */
+ 
