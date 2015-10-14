258b66e715d585f6fe88d211be63f8522127ee63
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-10-14 11:03:12 +0200

    Verifying problem on wake up threads on release all function.

--- hookomp.c
+++ hookomp.c
@@ -40,10 +40,10 @@ void foo(void) {
 void release_all_team_threads(void){
 	PRINT_FUNC_NAME;
 
-	int number_of_blocked_threads;
+	int number_of_blocked_threads = 0;
     sem_getvalue(&sem_blocks_other_team_threads, &number_of_blocked_threads);
     
-	TRACE("[HOOKOMP]: Waking up the %d blocked threads -> %d -- %d -- %d.\n", number_of_threads_in_team, omp_get_num_threads(), omp_get_thread_limit(), number_of_blocked_threads);
+	TRACE("[HOOKOMP]: Waking up the %d blocked threads -> %d.\n", number_of_threads_in_team, number_of_blocked_threads);
 	for (int i = 0; i < number_of_threads_in_team; ++i) {
 		sem_post(&sem_blocks_other_team_threads);
 	}
