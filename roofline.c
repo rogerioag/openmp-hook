61df62537af43b1d7402dc4122156b178eefbbbe
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-11-24 14:50:14 +0100

    Implementation of operational intensity calculation.

--- roofline.c
+++ roofline.c
@@ -441,7 +441,7 @@ bool RM_stop_and_accumulate(void){
 	int j;
 
   	TRACE("Trying accumulate counters.\n");
-	if ((retval = PAPI_accum(ptr_measure->EventSet, p&tr_measure->values[ptr_measure->current_eventset][0])) != PAPI_OK){
+	if ((retval = PAPI_accum(ptr_measure->EventSet, &ptr_measure->values[ptr_measure->current_eventset][0])) != PAPI_OK){
 		TRACE("PAPI_accum error: %d %s\n", retval, PAPI_strerror(retval));
 		RM_papi_handle_error(__FUNCTION__, retval, __LINE__);
 
