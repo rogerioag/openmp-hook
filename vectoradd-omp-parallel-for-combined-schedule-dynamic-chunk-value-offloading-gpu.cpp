409e54e9c2bec4ae9564071222507c61fee38337
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-12-01 13:04:42 +0100

    Fixing bugs in vectoradd-omp-parallel-for-combined-schedule-dynamic-chunk-value-offloading-gpu sample.
    Former-commit-id: 14507aad8edd9ee5241d6eea94024d67e3f3aabb

--- vectoradd-omp-parallel-for-combined-schedule-dynamic-chunk-value-offloading-gpu.cpp
+++ vectoradd-omp-parallel-for-combined-schedule-dynamic-chunk-value-offloading-gpu.cpp
@@ -279,23 +279,23 @@ void handler_function_main_GPU(void){
 }
 
 /*------------------------------------------------------------------------------*/
-bool create_target_functions_table(op_func **table, int nrows, int ncolumns){
+bool create_target_functions_table(op_func ***table, int nrows, int ncolumns){
 
   bool result = true;
   int i, j;
 
   fprintf(stderr, "Allocating the rows.\n");
-  table = (op_func **) malloc(nrows * sizeof(op_func *));
+  *table = (op_func **) malloc(nrows * sizeof(op_func *));
 
-  if(table == NULL){
+  if(*table == NULL){
     fprintf(stderr, "Error in table of target functions allocation (rows).\n");
     result= false;
   }
   else{
     fprintf(stderr, "Allocating the columns.\n");
     for(i = 0; i < nrows; i++){
-      table[i] = (op_func *) malloc(ncolumns * sizeof(op_func));
-      if(table [i] == NULL){
+      *table[i] = (op_func *) malloc(ncolumns * sizeof(op_func));
+      if(*table [i] == NULL){
         fprintf(stderr, "Error in table of target functions allocation (columns).\n");
         result = false;
       }
@@ -306,7 +306,7 @@ bool create_target_functions_table(op_func **table, int nrows, int ncolumns){
   fprintf(stderr, "Initializing.\n");
   for(i = 0; i < nrows; i++) {
     for(j = 0; j < ncolumns; j++){
-      table[i][j] = 0;
+      *table[i][j] = 0;
     }
   }
   fprintf(stderr, "Initializing OK.\n");
@@ -333,7 +333,7 @@ int main() {
   int nloops = 2;
   int ndevices = 1;
 
-  if(create_target_functions_table(getTargetFunc, nloops, ndevices)){
+  if(create_target_functions_table(&getTargetFunc, nloops, ndevices)){
     /* Set up the library Functions table. */
     if(getTargetFunc == NULL){
       fprintf(stderr, "Structure is NULL.\n");      
