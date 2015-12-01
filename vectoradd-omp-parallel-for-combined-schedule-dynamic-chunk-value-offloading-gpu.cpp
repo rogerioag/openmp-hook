9ff44ba66a926c2243f65320dc1c2a9756c2dde1
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-12-01 10:05:49 +0100

    Fixing bugs in vectoradd-omp-parallel-for-combined-schedule-dynamic-chunk-value-offloading-gpu sample.
    Former-commit-id: f4c4ee61fec5be3ccf89ea70bfd8823502086bd9

--- vectoradd-omp-parallel-for-combined-schedule-dynamic-chunk-value-offloading-gpu.cpp
+++ vectoradd-omp-parallel-for-combined-schedule-dynamic-chunk-value-offloading-gpu.cpp
@@ -277,7 +277,7 @@ bool create_target_functions_table(op_func **table, int nrows, int ncolumns){
   bool result = true;
   int i;
 
-  table = (op_func *) malloc(nrows * sizeof(op_func *));
+  table = (op_func **) malloc(nrows * sizeof(op_func **));
 
   if(table == NULL){
     fprintf(stderr, "Error in table of target functions allocation (rows).");
@@ -285,7 +285,7 @@ bool create_target_functions_table(op_func **table, int nrows, int ncolumns){
   }
   else{
     for(i = 0; i < nrows; i++){
-      table[i] = malloc(ncolumns * sizeof(op_func));
+      table[i] = (op_func *) malloc(ncolumns * sizeof(op_func*));
       if(table [i] == NULL){
         fprintf(stderr, "Error in table of target functions allocation (columns).");
         result = false;
