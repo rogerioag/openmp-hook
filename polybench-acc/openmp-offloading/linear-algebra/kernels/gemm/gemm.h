34bbb9d20101c9afc1a0af490c3c3bcd2b6142a4
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-12-03 14:40:12 +0100

    Adding again the polybench benchmarks.
    Former-commit-id: b3c9ee3443498b3f975f117bfc3919df58df41cc
    Former-commit-id: bbfdc7d32d8c284e1ec3e48933afff6daacc6df4

--- polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm.h
+++ polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm.h
@@ -1,10 +1,13 @@
 /**
- * gemm.h: This file is part of the PolyBench/C 3.2 test suite.
+ * gemm.cuh: This file is part of the PolyBench/GPU 1.0 test suite.
  *
  *
- * Contact: Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
- * Web address: http://polybench.sourceforge.net
+ * Contact: Scott Grauer-Gray <sgrauerg@gmail.com>
+ * Will Killian <killian@udel.edu>
+ * Louis-Noel Pouchet <pouchet@cse.ohio-state.edu>
+ * Web address: http://www.cse.ohio-state.edu/~pouchet/software/polybench/GPU
  */
+
 #ifndef GEMM_H
 # define GEMM_H
 
@@ -17,33 +20,33 @@
 # if !defined(NI) && !defined(NJ) && !defined(NK)
 /* Define the possible dataset sizes. */
 #  ifdef MINI_DATASET
-#   define NI 32
-#   define NJ 32
-#   define NK 32
+#define NI 128
+#define NJ 128
+#define NK 128
 #  endif
 
 #  ifdef SMALL_DATASET
-#   define NI 128
-#   define NJ 128
-#   define NK 128
+#define NI 256
+#define NJ 256
+#define NK 256
 #  endif
 
 #  ifdef STANDARD_DATASET /* Default if unspecified. */
-#   define NI 1024
-#   define NJ 1024
-#   define NK 1024
+#define NI 512
+#define NJ 512
+#define NK 512
 #  endif
 
 #  ifdef LARGE_DATASET
-#   define NI 2000
-#   define NJ 2000
-#   define NK 2000
+#define NI 1024
+#define NJ 1024
+#define NK 1024
 #  endif
 
 #  ifdef EXTRALARGE_DATASET
-#   define NI 4000
-#   define NJ 4000
-#   define NK 4000
+#define NI 2048
+#define NJ 2048
+#define NK 2048
 #  endif
 # endif /* !N */
 
@@ -56,5 +59,9 @@
 #  define DATA_PRINTF_MODIFIER "%0.2lf "
 # endif
 
+/* Thread block dimensions */
+#define DIM_THREAD_BLOCK_X 32
+#define DIM_THREAD_BLOCK_Y 8
 
-#endif /* !GEMM */
+
+#endif /* !GEMM*/
--- polybench-acc/openmp-offloading/utilities/polybench.c
+++ polybench-acc/openmp-offloading/utilities/polybench.c
@@ -380,14 +380,14 @@ static
 void *
 xmalloc (size_t num)
 {
-  void* new = NULL;
-  int ret = posix_memalign (&new, 32, num);
-  if (! new || ret)
+  void* newA = NULL;
+  int ret = posix_memalign (&newA, 32, num);
+  if (! newA || ret)
     {
       fprintf (stderr, "[PolyBench] posix_memalign: cannot allocate memory");
       exit (1);
     }
-  return new;
+  return newA;
 }
 
 
