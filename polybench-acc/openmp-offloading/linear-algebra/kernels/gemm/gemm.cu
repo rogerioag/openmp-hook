bde6eb72837965158723f17d1c7e46090b232227
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-12-07 15:11:22 +0100

    Updating the gemm cuda sample.
    Former-commit-id: ec0b615d1ea0ddf3ad07788b036306426e5a05b6
    Former-commit-id: eee483fcb4f4554c005e136c5645ccbc96265792

--- polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm.cu
+++ polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm.cu
@@ -363,12 +363,9 @@ int main(int argc, char *argv[]) {
         POLYBENCH_ARRAY(B),
         POLYBENCH_ARRAY(C));
 
-  gemm_omp(ni, nj, nk, alpha, beta, 
-        POLYBENCH_ARRAY(A), 
-        POLYBENCH_ARRAY(B),
-        POLYBENCH_ARRAY(C_outputFromOMP));
+  // gemm_omp(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C_outputFromOMP));
 
-  compareResults(ni, nj, POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(C_outputFromOMP));
+  // compareResults(ni, nj, POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(C_outputFromOMP));
   
   GPU_argv_init();
 
