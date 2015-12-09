69ae5bee1810cb7a6a109dc859ace30b320af68f
Rogério Gonçalves <rogerio.rag@gmail.com>
2015-12-09 16:01:36 +0100

    Adding benchmark using setjump/longjump
    Former-commit-id: 52764f7ba6a156ec62bc758ca50d1c4a2dd3f70e
    Former-commit-id: e882c584939f76dd58fb460f52483728925bb1c9

--- polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm-slj.cu
+++ polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm-slj.cu
@@ -122,6 +122,7 @@ void gemm_omp_kernel(int ni, int nj, int nk, DATA_TYPE alpha, DATA_TYPE beta,
     for (i = 0; i < _PB_NI; i++)
       for (j = 0; j < _PB_NJ; j++) {
         C[i][j] *= beta;
+        THROW;
         for (k = 0; k < _PB_NK; ++k)
           C[i][j] += alpha * A[i][k] * B[k][j];
       }
