f90358a3b930f3c9ba81adb4c9ac3f0c8f8fbb97
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-05 00:19:00 +0100

    Updating benchmark syr2k. Tests without hook.
    Former-commit-id: 6cd8c8ab40362633c22a8807fcd2213dc418b1df

--- tests/polybench-acc/openmp-offloading/utilities/common.mk
+++ tests/polybench-acc/openmp-offloading/utilities/common.mk
@@ -1,7 +1,8 @@
 INCLUDE_DIR := -I/home/goncalv/cuda/include -I/usr/include/c++/4.8 -I/usr/include/c++/4.8/x86_64-linux-gnu/ -I/home/goncalv/cuda/samples/common/inc
 
 LIBS_DIR := -L/home/goncalv/cuda/lib64 -L/home/goncalv/cuda/samples/common/lib -L . -L ../../../../../../
-LIBRARIES:= -lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
+# LIBRARIES:= -lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
+LIBRARIES:= -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
 
 all:
 	nvcc ${OPT_LEVEL} -c ${CUFILES} -I${PATH_TO_UTILS} ${INCLUDE_DIR} -DPOLYBENCH_TIME -ccbin=${CXX} -Xcompiler -fopenmp -Xcompiler -lgomp -Xcompiler -fpermissive -Xcompiler -lffi -Xcompiler -ldl -o ${OBJFILE} 
