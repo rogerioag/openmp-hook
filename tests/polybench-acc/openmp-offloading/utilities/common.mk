410e65666b0bfb351cf20ad41fdb63b1722c441d
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-04 10:38:56 +0100

    Update Makefile.
    Former-commit-id: 79c90cae68c8170cd3bf8b7fe5a7467d5f9f5f5a

--- tests/polybench-acc/openmp-offloading/utilities/common.mk
+++ tests/polybench-acc/openmp-offloading/utilities/common.mk
@@ -1,4 +1,12 @@
+INCLUDE_DIR := "-I/home/goncalv/cuda/include -I/usr/include/c++/4.8 -I/usr/include/c++/4.8/x86_64-linux-gnu/ -I/home/goncalv/cuda/samples/common/inc"
+
+LIBS_DIR := "-L/home/goncalv/cuda/lib64 -L/home/goncalv/cuda/samples/common/lib -L."
+LIBRARIES:= "-lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart"
+
 all:
-	nvcc -O3 ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE} 
+	nvcc -O3 -c ${CUFILES} -I${PATH_TO_UTILS} -o ${OBJTFILE} ${INCLUDE_DIR} -DPOLYBENCH_TIME -ccbin=${CXX} -Xcompiler -fpermissive
+	${CXX} -g ${OBJTFILE} -o ${EXECUTABLE} -O3 ${LIBS_DIR} ${LIBRARIES}
+
 clean:
 	rm -f *~ *.exe
+
