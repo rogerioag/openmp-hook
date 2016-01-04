fc764749e1896358c35e2b3728d9a44d8f72c3ad
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-04 10:52:34 +0100

    Update Makefile.
    Former-commit-id: 2f5f86ac2c2e63483d5b2772de4ae19a114a3ed3
    Former-commit-id: 8305cebec9ff7be0dcb61b662a0f42e06d2af10b

--- tests/polybench-acc/openmp-offloading/utilities/common.mk
+++ tests/polybench-acc/openmp-offloading/utilities/common.mk
@@ -1,7 +1,7 @@
-INCLUDE_DIR := "-I/home/goncalv/cuda/include -I/usr/include/c++/4.8 -I/usr/include/c++/4.8/x86_64-linux-gnu/ -I/home/goncalv/cuda/samples/common/inc"
+INCLUDE_DIR := -I/home/goncalv/cuda/include -I/usr/include/c++/4.8 -I/usr/include/c++/4.8/x86_64-linux-gnu/ -I/home/goncalv/cuda/samples/common/inc
 
-LIBS_DIR := "-L/home/goncalv/cuda/lib64 -L/home/goncalv/cuda/samples/common/lib -L . -L ../../../../../../"
-LIBRARIES:= "-lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart"
+LIBS_DIR := -L/home/goncalv/cuda/lib64 -L/home/goncalv/cuda/samples/common/lib -L . -L ../../../../../../
+LIBRARIES:= -lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
 
 all:
 	nvcc -O3 -c ${CUFILES} -I${PATH_TO_UTILS} ${INCLUDE_DIR} -DPOLYBENCH_TIME -ccbin=${CXX} -Xcompiler -fpermissive -o ${OBJFILE} 
