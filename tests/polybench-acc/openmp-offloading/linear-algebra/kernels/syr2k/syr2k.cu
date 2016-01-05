04ada72eef03579d3df4a02e488fe7c83367256c
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-05 01:26:45 +0100

    Updating benchmark syr2k. Tests without hook.
    Former-commit-id: 57c8b83dd612df1326353f7443897847219f7c4e
    Former-commit-id: 90ae729825089781a279fd6dbe2cc9072a8ec90e

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/syr2k/syr2k.cu
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/syr2k/syr2k.cu
@@ -283,7 +283,7 @@ int main(int argc, char *argv[]) {
 
   /*Copy the original C to C of OMP.*/
   // memcpy(C_outputFromOMP, C, sizeof(C_outputFromOMP));
-  copy_array(ni, C, POLYBENCH_ARRAY(C_outputFromOMP));
+  copy_array(ni, POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(C_outputFromOMP));
   C[0][0] = 24.24;
   printf("%4.2f - %4.2f\n", C[0][0], C_outputFromOMP[0][0]);
 
