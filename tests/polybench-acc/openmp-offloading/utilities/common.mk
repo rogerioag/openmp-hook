d8e9c9d8de90cc1e46b1e439e49a3c33904ad05f
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-05 14:38:02 +0100

    Extracting offloading support to header file offload.h
    Former-commit-id: 3e3a25df1149ee14947c5dd455583f6941e463d3
    Former-commit-id: f8c8b72a05e0a4c6c24da743e9d55cb3988358df

--- tests/polybench-acc/openmp-offloading/utilities/common.mk
+++ tests/polybench-acc/openmp-offloading/utilities/common.mk
@@ -1,8 +1,7 @@
-INCLUDE_DIR := -I/home/goncalv/cuda/include -I/usr/include/c++/4.8 -I/usr/include/c++/4.8/x86_64-linux-gnu/ -I/home/goncalv/cuda/samples/common/inc
+INCLUDE_DIR := -I/home/goncalv/prova-de-conceito/testes-prova-conceito/openmp-hook -I/home/goncalv/cuda/include -I/usr/include/c++/4.8 -I/usr/include/c++/4.8/x86_64-linux-gnu/ -I/home/goncalv/cuda/samples/common/inc
 
-LIBS_DIR := -L/home/goncalv/cuda/lib64 -L/home/goncalv/cuda/samples/common/lib -L . -L ../../../../../../
-# LIBRARIES:= -lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
-LIBRARIES:= -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
+LIBS_DIR := -L/home/goncalv/prova-de-conceito/testes-prova-conceito/openmp-hook -L/home/goncalv/cuda/lib64 -L/home/goncalv/cuda/samples/common/lib -L . -L ../../../../../../
+LIBRARIES:= -lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
 
 all:
 	nvcc ${OPT_LEVEL} -c ${CUFILES} -I${PATH_TO_UTILS} ${INCLUDE_DIR} -DPOLYBENCH_TIME -ccbin=${CXX} -Xcompiler -fopenmp -Xcompiler -lgomp -Xcompiler -fpermissive -Xcompiler -lffi -Xcompiler -ldl -o ${OBJFILE} 
