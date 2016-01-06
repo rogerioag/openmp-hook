09e558d05ab258bc46812fc08122b49c7a28c95c
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-06 21:14:55 +0100

    Updating 2mm benchmark.
    Former-commit-id: 04a67dc43077bde71714870114dc04dd2ad92ad4
    Former-commit-id: 062ac43ca252a30b7d9c067a5816774a7f01ca7d

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/2mm/2mm.cu
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/2mm/2mm.cu
@@ -571,13 +571,14 @@ int main(int argc, char **argv) {
           POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(D));
 
   fprintf(stderr, "Calling gemm_omp.\n");
-  mm_omp(ni, nj, nk, nl,
+  /*mm_omp(ni, nj, nk, nl,
 	      alpha, beta,
 	      POLYBENCH_ARRAY(tmp),
 	      POLYBENCH_ARRAY(A),
 	      POLYBENCH_ARRAY(B),
 	      POLYBENCH_ARRAY(C),
 	      POLYBENCH_ARRAY(D_outputFromOMP));
+   */
 
   fprintf(stderr, "Calling compareResults(original, omp).\n");
   compareResults(ni, nl, POLYBENCH_ARRAY(D), POLYBENCH_ARRAY(D_outputFromOMP));
@@ -587,8 +588,11 @@ int main(int argc, char **argv) {
   //        POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(D),
   //        POLYBENCH_ARRAY(D_outputFromGpu));
 
-  // fprintf(stderr, "Calling gemm_cuda using Table of Pointers.\n");
-  // call_function_ffi_call(table[0][0]);
+  fprintf(stderr, "Calling mm2Cuda_1 using Table of Pointers.\n");
+  call_function_ffi_call(table[0][0]);
+
+  fprintf(stderr, "Calling mm2Cuda_2 using Table of Pointers.\n");
+  call_function_ffi_call(table[1][0]);
 
   fprintf(stderr, "Calling compareResults(original, cuda).\n");
   compareResults(ni, nl, POLYBENCH_ARRAY(D), POLYBENCH_ARRAY(D_outputFromGpu));
