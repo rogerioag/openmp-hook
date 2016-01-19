89a88ddda913ef2c27b082f2ee6982486fcc8f06
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-19 21:55:11 +0100

    Preparing OpenMP benchmarks version.
    Former-commit-id: f248cca656c71aa59267523752adb2dfc8573531

--- tests/polybench-acc/OpenCL/utilities/common.mk
+++ tests/polybench-acc/OpenCL/utilities/common.mk
@@ -1,6 +1,12 @@
-OpenCL_SDK=/global/homes/s/sgrauerg/NVIDIA_GPU_Computing_SDK
-INCLUDE=-I${OpenCL_SDK}/OpenCL/common/inc -I${PATH_TO_UTILS}
-LIBPATH=-L${OpenCL_SDK}/OpenCL/common/lib -L${OpenCL_SDK}/shared/lib
+# OpenCL_SDK=/global/homes/s/sgrauerg/NVIDIA_GPU_Computing_SDK
+OpenCL_SDK=/home/goncalv/cuda
+
+# INCLUDE=-I${OpenCL_SDK}/OpenCL/common/inc -I${PATH_TO_UTILS}
+INCLUDE=-I${OpenCL_SDK}/include -I${PATH_TO_UTILS}
+
+#LIBPATH=-L${OpenCL_SDK}/OpenCL/common/lib -L${OpenCL_SDK}/shared/lib
+LIBPATH=-L${OpenCL_SDK}/lib64 -L${OpenCL_SDK}/lib
+
 LIB=-lOpenCL -lm
 all:
 	gcc -O3 ${INCLUDE} ${LIBPATH} ${LIB} ${CFILES} -o ${EXECUTABLE}
--- tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/gemm.c
+++ tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/gemm.c
@@ -14,11 +14,14 @@
 
 /* Include polybench common header. */
 #include <polybench.h>
+ #include <polybenchUtilFuncts.h>
 
 /* Include benchmark-specific header. */
 /* Default data type is double, default size is 4000. */
 #include "gemm.h"
 
+// define the error threshold for the results "not matching"
+#define PERCENT_DIFF_ERROR_THRESHOLD 0.05
 
 /* Array initialization. */
 static
@@ -61,37 +64,113 @@ void print_array(int ni, int nj,
   fprintf (stderr, "\n");
 }
 
+/* ------------------------------------------------------------- */
+void compareResults(int ni, int nj, DATA_TYPE POLYBENCH_2D(C, NI, NJ, ni, nj),
+                    DATA_TYPE POLYBENCH_2D(C_output, NI, NJ, ni, nj)) {
+  int i, j, fail;
+  fail = 0;
 
+  // Compare CPU and GPU outputs
+  for (i = 0; i < ni; i++) {
+    for (j = 0; j < nj; j++) {
+      if (percentDiff(C[i][j], C_output[i][j]) >
+          PERCENT_DIFF_ERROR_THRESHOLD) {
+        fail++;
+      }
+    }
+  }
+
+  // Print results
+  printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f "
+         "Percent: %d\n",
+         PERCENT_DIFF_ERROR_THRESHOLD, fail);
+}
+
+/* ------------------------------------------------------------- */
+/* Original Version. */
+void gemm(int ni, int nj, int nk, DATA_TYPE alpha, DATA_TYPE beta,
+          DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
+          DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
+          DATA_TYPE POLYBENCH_2D(C, NI, NJ, ni, nj)) {
+  int i, j, k;
+
+  for (i = 0; i < _PB_NI; i++) {
+    for (j = 0; j < _PB_NJ; j++) {
+      C[i][j] *= beta;
+
+      for (k = 0; k < _PB_NK; ++k) {
+        C[i][j] += alpha * A[i][k] * B[k][j];
+      }
+    }
+  }
+}
+
+
+/* ------------------------------------------------------------- */
+void gemm_original(int ni, int nj, int nk, DATA_TYPE alpha, DATA_TYPE beta,
+          DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
+          DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
+          DATA_TYPE POLYBENCH_2D(C, NI, NJ, ni, nj)) {
+  
+  /* Start timer. */
+  polybench_start_instruments;
+
+  gemm(ni, nj, nk, alpha, beta, 
+        A, 
+        B,
+        C);
+
+  /* Stop and print timer. */
+  printf("Original CPU Time in seconds:\n");
+  polybench_stop_instruments;
+  polybench_print_instruments;
+}
+
+/* ------------------------------------------------------------- */
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
-static
-void kernel_gemm(int ni, int nj, int nk,
-		 DATA_TYPE alpha,
-		 DATA_TYPE beta,
-		 DATA_TYPE POLYBENCH_2D(C,NI,NJ,ni,nj),
-		 DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
-		 DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj))
-{
+void gemm_omp_kernel(int ni, int nj, int nk, DATA_TYPE alpha, DATA_TYPE beta,
+          DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
+          DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
+          DATA_TYPE POLYBENCH_2D(C, NI, NJ, ni, nj)) {
+
   int i, j, k;
   #pragma scop
   #pragma omp parallel
   {
-    /* C := alpha*A*B + beta*C */
-    #pragma omp for private (j, k)
+  /* C := alpha*A*B + beta*C */
+  #pragma omp for private(j, k) schedule(runtime)
     for (i = 0; i < _PB_NI; i++)
-      for (j = 0; j < _PB_NJ; j++)
-	{
-	  C[i][j] *= beta;
-	  for (k = 0; k < _PB_NK; ++k)
-	    C[i][j] += alpha * A[i][k] * B[k][j];
-	}
+      for (j = 0; j < _PB_NJ; j++) {
+        C[i][j] *= beta;
+        for (k = 0; k < _PB_NK; ++k)
+          C[i][j] += alpha * A[i][k] * B[k][j];
+      }
   }
   #pragma endscop
 }
+/* ------------------------------------------------------------- */
+void gemm_omp(int ni, int nj, int nk, DATA_TYPE alpha, DATA_TYPE beta,
+              DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
+              DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
+              DATA_TYPE POLYBENCH_2D(C_outputFromOMP, NI, NJ, ni, nj)) {
 
+  /* Start timer. */
+  polybench_start_instruments;
 
-int main(int argc, char** argv)
-{
+  gemm_omp_kernel(ni, nj, nk, alpha, beta, 
+                  A, 
+                  B,
+                  C_outputFromOMP);
+
+  /* Stop and print timer. */
+  printf("OpenMP Time in seconds:\n");
+  polybench_stop_instruments;
+  polybench_print_instruments;
+}
+
+/* ------------------------------------------------------------- */
+int main(int argc, char *argv[]) {
   /* Retrieve problem size. */
   int ni = NI;
   int nj = NJ;
@@ -100,38 +179,33 @@ int main(int argc, char** argv)
   /* Variable declaration/allocation. */
   DATA_TYPE alpha;
   DATA_TYPE beta;
-  POLYBENCH_2D_ARRAY_DECL(C,DATA_TYPE,NI,NJ,ni,nj);
-  POLYBENCH_2D_ARRAY_DECL(A,DATA_TYPE,NI,NK,ni,nk);
-  POLYBENCH_2D_ARRAY_DECL(B,DATA_TYPE,NK,NJ,nk,nj);
+  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, NI, NK, ni, nk);
+  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, NK, NJ, nk, nj);
+  POLYBENCH_2D_ARRAY_DECL(C, DATA_TYPE, NI, NJ, ni, nj);
+  POLYBENCH_2D_ARRAY_DECL(C_outputFromOMP, DATA_TYPE, NI, NJ, ni, nj);
+  
+  fprintf(stderr, "Calling init_array.\n");
+  init_array(ni, nj, nk, &alpha, &beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B),
+       POLYBENCH_ARRAY(C));
 
-  /* Initialize array(s). */
-  init_array (ni, nj, nk, &alpha, &beta,
-	      POLYBENCH_ARRAY(C),
-	      POLYBENCH_ARRAY(A),
-	      POLYBENCH_ARRAY(B));
+  /*Copy the original C to C of OMP.*/
+  memcpy(C_outputFromOMP, C, sizeof(C_outputFromOMP));
 
-  /* Start timer. */
-  polybench_start_instruments;
+  fprintf(stderr, "Calling gemm_original.\n");
+  gemm_original(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C));
+  
+  fprintf(stderr, "Calling gemm_omp.\n");
+  gemm_omp(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C_outputFromOMP));
 
-  /* Run kernel. */
-  kernel_gemm (ni, nj, nk,
-	       alpha, beta,
-	       POLYBENCH_ARRAY(C),
-	       POLYBENCH_ARRAY(A),
-	       POLYBENCH_ARRAY(B));
+  fprintf(stderr, "Calling compareResults(original, omp).\n");
+  compareResults(ni, nj, POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(C_outputFromOMP));
 
-  /* Stop and print timer. */
-  polybench_stop_instruments;
-  polybench_print_instruments;
+  polybench_prevent_dce(print_array(ni, nj, POLYBENCH_ARRAY(C_outputFromOMP)));
 
-  /* Prevent dead-code elimination. All live-out data must be printed
-     by the function call in argument. */
-  polybench_prevent_dce(print_array(ni, nj,  POLYBENCH_ARRAY(C)));
-
-  /* Be clean. */
-  POLYBENCH_FREE_ARRAY(C);
   POLYBENCH_FREE_ARRAY(A);
   POLYBENCH_FREE_ARRAY(B);
+  POLYBENCH_FREE_ARRAY(C);
+  POLYBENCH_FREE_ARRAY(C_outputFromOMP);
 
   return 0;
 }
