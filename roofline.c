add6826ff67683e817a4f7eb33cfdcc9bb61be09
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-26 01:52:36 +0100

    Adding attainable performance calculate.
    Former-commit-id: 2b6a4d7ede4af4c203a9e8d7e3c3c875c441f802
    Former-commit-id: 753b5d74ecf9012b23227295e76fc3df519130b3

--- roofline.c
+++ roofline.c
@@ -876,7 +876,7 @@ double RM_attainable_performance(int id_device, double op_intensity){
 
 	TRACE("Calculating Attainable Performance to device %d with Operational intensity %10.6f\n", id_device, op_intensity);
 	// Attainable performance = Min( F_flops, B_mem * I).
-	ap = min(devices[id_device].flops, (devices[id_device].bandwidth * op_intensity));
+	ap = MIN(devices[id_device].flops, (devices[id_device].bandwidth * op_intensity));
 	
 	TRACE("AP: %10.6f\n", ap);
 
