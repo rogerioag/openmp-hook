440548190f098fdb97cdfbd15ccfbdaca02822da
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-01-27 20:03:11 +0100

    Updating L1 to use perf linux events.
    Former-commit-id: 288f6e000b50b035841574f1aaa42e20a43c89b7
    Former-commit-id: 4ed957006fed11ce26867ee47550776719f27c7f

--- roofline.h
+++ roofline.h
@@ -50,9 +50,11 @@ static char *event_names[NUM_EVENT_SETS][NUM_MAX_EVENTS] = {
 /* MEM_event_names */{ "UNC_H_IMC_READS", "UNC_H_IMC_WRITES", NULL, NULL, NULL },
 /* L3_event_names */ { "PAPI_TOT_CYC", "PAPI_REF_CYC", "PAPI_L3_TCR", "PAPI_L3_TCW", NULL },
 /* L2_event_names */ { "PAPI_TOT_CYC", "PAPI_REF_CYC", "PAPI_L2_TCR", "PAPI_L2_TCW", NULL },
-/* L1_event_names */ { "PAPI_TOT_CYC", "PAPI_REF_CYC", "PAPI_L1_TCR", "PAPI_L1_TCW", NULL },
+///* L1_event_names */ { "PAPI_TOT_CYC", "PAPI_REF_CYC", "PAPI_L1_TCR", "PAPI_L1_TCW", NULL },
+/* L1_event_names */ { "UNHALTED_CORE_CYCLES", "UNHALTED_REFERENCE_CYCLES", "perf::PERF_COUNT_HW_CACHE_L1D:READ", "perf::PERF_COUNT_HW_CACHE_L1D:WRITE", NULL },
 /* FPO_event_names */{ "PAPI_TOT_CYC", "PAPI_REF_CYC", "PAPI_DP_OPS", 		 NULL, NULL }
 };
+
 
 /* The kind of component the eventset was associated. Is need different event sets to measures preset events and native events. 
    Components cpu (0) and perf_event_uncore to uncore events are associated with EventSet. 
