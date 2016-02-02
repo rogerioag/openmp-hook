40c4efa21429778cf911420ce34da24fa18885aa
rogerio.rag <rogerio.rag@gmial.com>
2016-02-02 10:12:37 +0100

    Updating the Makefile to support parameters.
    Former-commit-id: ddf44747dfde9851c6fa597d5c8bfdc9ca74a32f
    Former-commit-id: e9e6fbbfe2b42aacbfc26c81f9866260497ef870

--- tests/polybench-acc/openmp-offloading/utilities/common.mk
+++ tests/polybench-acc/openmp-offloading/utilities/common.mk
@@ -4,10 +4,16 @@ LIBS_DIR := -L/home/goncalv/prova-de-conceito/testes-prova-conceito/openmp-hook 
 LIBRARIES:= -lhookomp -lroofline -fopenmp -lgomp -lffi -ldl -lcuda -lcudart
 
 all:
-	nvcc ${OPT_LEVEL} -c ${CUFILES} -I${PATH_TO_UTILS} ${INCLUDE_DIR} ${MAKE_OPTIONS} -ccbin=${CXX} -Xcompiler -lhookomp -Xcompiler -lroofline -Xcompiler -fopenmp -Xcompiler -lgomp -Xcompiler -fpermissive -Xcompiler -lffi -Xcompiler -ldl -o ${OBJFILE} 
+	${NVCC} ${OPT_LEVEL} -c ${CUFILES} -I${PATH_TO_UTILS} ${INCLUDE_DIR} ${POLYBENCH_OPTIONS} ${OMP_CONFIG} -ccbin=${CXX} -Xcompiler -lhookomp -Xcompiler -lroofline -Xcompiler -fopenmp -Xcompiler -lgomp -Xcompiler -fpermissive -Xcompiler -lffi -Xcompiler -ldl -o ${OBJFILE} 
 
 	${CXX} -g ${OBJFILE} ${OPT_LEVEL} ${LIBS_DIR} ${LIBRARIES} -o ${EXECUTABLE}
 
 clean:
 	rm -f *~ *.o *.exe
 
+info:
+	@echo "Use:"
+	@echo "make POLYBENCH_OPTIONS=\"-DPOLYBENCH_TIME -DEXTRALARGE_DATASET\" OMP_CONFIG=\"-DOPENMP_SCHEDULE_RUNTIME -DOPENMP_CHUNK_SIZE=64 -DOPENMP_NUM_THREADS=24\""
+	@echo "make POLYBENCH_OPTIONS=\"-DPOLYBENCH_TIME -DEXTRALARGE_DATASET\" OMP_CONFIG=\"-DOPENMP_SCHEDULE_DYNAMIC -DOPENMP_CHUNK_SIZE=64 -DOPENMP_NUM_THREADS=24\""
+	@echo "make POLYBENCH_OPTIONS=\"-DPOLYBENCH_TIME -DEXTRALARGE_DATASET\" OMP_CONFIG=\"-DOPENMP_SCHEDULE_GUIDED -DOPENMP_CHUNK_SIZE=64 -DOPENMP_NUM_THREADS=24\""
+	@echo "MINI_DATASET:32, SMALL_DATASET:128, STANDARD_DATASET:1024, LARGE_DATASET:2000, EXTRALARGE_DATASET:4000"
