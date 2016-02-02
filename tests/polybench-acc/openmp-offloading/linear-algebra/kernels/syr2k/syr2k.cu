f71a450bc6410d990510bc6248efbf853d910e58
rogerio.rag <rogerio.rag@gmial.com>
2016-02-02 16:16:12 +0100

    Updating syr2k barrier between loops.
    Former-commit-id: 256b2d3c935ea9e991355f0c1409242b40565eee

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/syr2k/syr2k.cu
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/syr2k/syr2k.cu
@@ -154,14 +154,14 @@ static void syr2k_omp_kernel(int ni, int nj, DATA_TYPE alpha, DATA_TYPE beta,
     // Copy back C.
     q_data_transfer_read = (sizeof(DATA_TYPE) * NI * NI);
     // #pragma omp for private(j) schedule(runtime)
-    #pragma omp for private(j, current_loop_index) schedule(OPENMP_SCHEDULE_WITH_CHUNK)
+    #pragma omp for private(j) schedule(OPENMP_SCHEDULE_WITH_CHUNK)
     for (i = 0; i < _PB_NI; i++)
       for (j = 0; j < _PB_NI; j++)
         C[i][j] *= beta;
   // }
-
+    
     #pragma omp barrier
-  
+
     current_loop_index = 1;
     // Copy to device A, B, C.
     q_data_transfer_write = (sizeof(DATA_TYPE) * NI * NJ) + (sizeof(DATA_TYPE) * NI * NJ) + (sizeof(DATA_TYPE) * NI * NI);
@@ -170,7 +170,7 @@ static void syr2k_omp_kernel(int ni, int nj, DATA_TYPE alpha, DATA_TYPE beta,
   // #pragma omp parallel num_threads(OPENMP_NUM_THREADS)
   // {
     // #pragma omp for private(j, k) schedule(runtime)
-    #pragma omp for private(j, k, current_loop_index) schedule(OPENMP_SCHEDULE_WITH_CHUNK)
+    #pragma omp for private(j, k) schedule(OPENMP_SCHEDULE_WITH_CHUNK)
     for (i = 0; i < _PB_NI; i++)
       for (j = 0; j < _PB_NI; j++)
         for (k = 0; k < _PB_NJ; k++) {
