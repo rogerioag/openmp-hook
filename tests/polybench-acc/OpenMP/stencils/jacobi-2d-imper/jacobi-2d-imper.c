b0801ae42fd6d505719cf7dd54770bdd70d6fd9a
rogerio.rag <rogerio.rag@gmial.com>
2016-02-06 10:34:31 +0100

    Preparing the jacobi2d benchmark.
    Former-commit-id: 6043ef7d72303995f90a2270a5aa708c50913813
    Former-commit-id: bb4ec62813bf3446f8463c569a76efa0c9ce538c

--- tests/polybench-acc/OpenMP/stencils/jacobi-2d-imper/jacobi-2d-imper.c
+++ tests/polybench-acc/OpenMP/stencils/jacobi-2d-imper/jacobi-2d-imper.c
@@ -92,16 +92,18 @@ void compareResults(int n, DATA_TYPE POLYBENCH_2D(A,N,N,n,n), DATA_TYPE POLYBENC
 
 /* ------------------------------------------------------------- */
 void runJacobi2DCpu(int tsteps, int n, DATA_TYPE POLYBENCH_2D(A,N,N,n,n), DATA_TYPE POLYBENCH_2D(B,N,N,n,n)) {
+
+  int t, i, j;
   
-  for (int t = 0; t < _PB_TSTEPS; t++) {
-      for (int i = 1; i < _PB_N - 1; i++) {
-      for (int j = 1; j < _PB_N - 1; j++) {
+  for (t = 0; t < _PB_TSTEPS; t++) {
+    for (i = 1; i < _PB_N - 1; i++) {
+      for (j = 1; j < _PB_N - 1; j++) {
           B[i][j] = 0.2f * (A[i][j] + A[i][(j-1)] + A[i][(1+j)] + A[(1+i)][j] + A[(i-1)][j]);
       }
     }
     
-      for (int i = 1; i < _PB_N-1; i++) {
-      for (int j = 1; j < _PB_N-1; j++) {
+    for (i = 1; i < _PB_N-1; i++) {
+      for (j = 1; j < _PB_N-1; j++) {
           A[i][j] = B[i][j];
       }
     }
