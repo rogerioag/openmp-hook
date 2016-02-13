6d3c658e22a2a588d929a69e43c4ebb9ed338eb3
rogerio.rag <rogerio.rag@gmial.com>
2016-02-13 16:10:02 +0100

    print results csv.
    Former-commit-id: ca722a38eeac5d0fed039d41594382a526e3c6e8

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm.cu
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm.cu
@@ -389,11 +389,11 @@ int main(int argc, char *argv[]) {
 
   fprintf(stderr, "ORIG; ");
   gemm_original(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C));
-  fprintf(stderr, ";");
+  fprintf(stderr, "\b;");
   
-  fprintf(stderr, "OMP;");
+  fprintf(stderr, "OMP; ");
   gemm_omp(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C_outputFromOMP));
-  fprintf(stderr, ";\n");
+  fprintf(stderr, "\b;\n");
 
   fprintf(stderr, "Calling compareResults(original, omp).\n");
   compareResults(ni, nj, POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(C_outputFromOMP));
