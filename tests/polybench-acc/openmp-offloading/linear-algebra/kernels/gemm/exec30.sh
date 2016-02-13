5513eb71e9c82dda5a8c2a83b7ee3368783f735f
rogerio.rag <rogerio.rag@gmial.com>
2016-02-13 18:35:47 +0100

    Updating exec30 script.
    Former-commit-id: 72aa45090508de846f6ddc2b3a45b3fcfb90e20f

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 # recupera o nome do diretorio, que é o nome do benchmark.
-benchmark=basename `pwd`
+
+benchmark=`basename $PWD`
 
 # recupera o nome do diretorio, que é o nome do benchmark.
 echo "Executing $benchmark."
 
-for ((  i = 1 ;  i <= 30;  i++  ))
-do
-    for num_threads in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
-        echo "Execution ${i} of $benchmark."
+for num_threads in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
+    for ((  i = 1 ;  i <= 30;  i++  ))
+    do
+        echo "Execution ${i} of ${benchmark} with ${num_threads} threads."
         make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -DLARGE_DATASET" OMP_CONFIG="-DOPENMP_SCHEDULE_DYNAMIC -DOPENMP_CHUNK_SIZE=1 -DOPENMP_NUM_THREADS=${num_threads}"
         ./${benchmark}-offloading-gpu.exe >> data.csv
     done
