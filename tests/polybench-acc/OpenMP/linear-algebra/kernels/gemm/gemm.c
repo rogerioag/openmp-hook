0c1f8e71e6e123b59ced410d0867165894d71c62
rogerio.rag <rogerio.rag@gmial.com>
2016-02-15 00:10:39 +0100

    Merging the repositories.
    Former-commit-id: e7d5f558a59bb6dd1e5beee0200144d04e01a17c
    Former-commit-id: c298b83ebbbb554fa59a7d489c49bdd0f7ba2b71

--- tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/gemm.c
+++ tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/gemm.c
@@ -195,11 +195,17 @@ int main(int argc, char *argv[]) {
   /*Copy the original C to C of OMP.*/
   memcpy(C_outputFromOMP, C, sizeof(C_outputFromOMP));
 
+  fprintf(stdout, "num_threads, NI, NJ, NK, ORIG, OMP\n");
+
+  fprintf(stdout, "%d, %d, %d, %d, ", OPENMP_NUM_THREADS, NI, NJ, NK);
+
   fprintf(stderr, "Calling gemm_original.\n");
   gemm_original(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C));
+  fprintf(stdout, ", ");
   
   fprintf(stderr, "Calling gemm_omp.\n");
   gemm_omp(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C_outputFromOMP));
+  fprintf(stdout, "\n");
 
   fprintf(stderr, "Calling compareResults(original, omp).\n");
   compareResults(ni, nj, POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(C_outputFromOMP));
