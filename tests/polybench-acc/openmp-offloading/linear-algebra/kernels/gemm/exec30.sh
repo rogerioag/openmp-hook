94748847e2dbc7cb4f2be21f0e2f9d3ff3dfe61e
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-02-15 11:38:13 +0100

    Merging repositories
    Former-commit-id: 019b7f24c988f2a9ce50dfe725b69ad08e690585
    Former-commit-id: 5e02563fc2b614db8c7df123c12e52b11b7169fb

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
@@ -8,16 +8,22 @@ benchmark=`basename $PWD`
 echo "Executing $benchmark."
 
 for size_of_data in LARGE_DATASET; do
-    for num_threads in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
-        echo "Compiling ${benchmark} for ${num_threads} threads."
+    for num_threads in 64 32 24 22 20 18 16 12 8 4 2 1; do
+        echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
+        for omp_schedule in DYNAMIC; do
+            for chunk_size in 32 64 128; do
+                make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_${omp_schedule} -DOPENMP_CHUNK_SIZE=${chunk_size} -DOPENMP_NUM_THREADS=${num_threads}"
+       		mv ${benchmark}-offloading-gpu.exe ${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe
 
-        make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_DYNAMIC -DOPENMP_CHUNK_SIZE=1 -DOPENMP_NUM_THREADS=${num_threads}"
-
-        for ((  i = 1 ;  i <= 30;  i++  ))
-        do
-            echo "Execution ${i} of ${benchmark} with ${num_threads} threads."
-            ./${benchmark}-offloading-gpu.exe >> data.csv
+       		for ((  i = 1 ;  i <= 10;  i++  ))
+       		do
+			echo "Execution ${i} of ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
+           		./${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe >> data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.csv
+     		done
+    	     done
         done
     done
 done
 echo "End of process."
+
+
