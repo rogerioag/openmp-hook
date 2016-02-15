16517f4ec5ed845f235291c9b34506597c264511
rogerio.rag <rogerio.rag@gmial.com>
2016-02-16 00:39:04 +0100

    Improving the output gemm.
    Former-commit-id: efb9d37c5212ebfe32434733ad31c058cff1326b

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
@@ -8,17 +8,17 @@ benchmark=`basename $PWD`
 echo "Executing $benchmark."
 
 for size_of_data in LARGE_DATASET; do
-	for num_threads in 64 32 24 22 20 18 16 12 8 4 2 1; do
+	for num_threads in 24 22 20 18 16 14 12 10 8 6 4 2 1; do
 		echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
 		for omp_schedule in DYNAMIC; do
-			for chunk_size in 32 64 128; do
-					make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_${omp_schedule} -DOPENMP_CHUNK_SIZE=${chunk_size} -DOPENMP_NUM_THREADS=${num_threads}"
-					mv ${benchmark}-offloading-gpu.exe ${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe
-          for ((  i = 1 ;  i <= 10;  i++  ))
-					do
-            echo "Execution ${i} of ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
-						./${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe >> data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.csv
-				  done
+			for chunk_size in 16 32 64 128; do
+				make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_${omp_schedule} -DOPENMP_CHUNK_SIZE=${chunk_size} -DOPENMP_NUM_THREADS=${num_threads}"
+				mv ${benchmark}-offloading-gpu.exe ${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe
+          		for ((  i = 1 ;  i <= 10;  i++  ))
+				do
+            		echo "Execution ${i} of ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
+					./${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe >> data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.csv
+				done
 			done
 		done
 	done
