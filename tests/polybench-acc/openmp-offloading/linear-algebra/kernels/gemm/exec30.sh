a02d860e7b97be612cfca76980461aa5a11f7b56
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-02-16 15:11:08 +0100

    Old experiments.
    Former-commit-id: f88e72ce7ce0d181751aca52d7125e04ed7c0a95
    Former-commit-id: 86081f6ea4af8b71a670013741699be562c4d737

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
@@ -8,10 +8,10 @@ benchmark=`basename $PWD`
 echo "Executing $benchmark."
 
 for size_of_data in LARGE_DATASET; do
-	for num_threads in 1 24 22 20 18 16 14 12 10 8 6 4 2; do
+	for num_threads in 1 2 4 8 10 12; do
 		echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
 		for omp_schedule in DYNAMIC; do
-			for chunk_size in 16 32 64 128; do
+			for chunk_size in 16 32 64; do
 				make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_${omp_schedule} -DOPENMP_CHUNK_SIZE=${chunk_size} -DOPENMP_NUM_THREADS=${num_threads}"
 				mv ${benchmark}-offloading-gpu.exe ${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe
           		for ((  i = 1 ;  i <= 10;  i++  ))
