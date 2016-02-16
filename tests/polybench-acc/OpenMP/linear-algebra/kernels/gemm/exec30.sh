868cf28d1aa214b9e0f799bcfb8780de54300039
rogerio.rag <rogerio.rag@gmial.com>
2016-02-16 01:28:28 +0100

    Updating the exec30.sh
    Former-commit-id: 072754535f8072622ca2a85eaa5fbd6d8ce163c4

--- tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/exec30.sh
+++ tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/exec30.sh
@@ -8,10 +8,10 @@ benchmark=`basename $PWD`
 echo "Executing $benchmark."
 
 for size_of_data in LARGE_DATASET; do
-    for num_threads in 64 32 24 22 20 18 16 12 8 4 2 1; do
+    for num_threads in 1 24 22 20 18 16 14 12 10 8 6 4 2; do
         echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
         for omp_schedule in DYNAMIC; do
-            for chunk_size in 32 64 128; do
+            for chunk_size in 16 32 64 128; do
                 make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_${omp_schedule} -DOPENMP_CHUNK_SIZE=${chunk_size} -DOPENMP_NUM_THREADS=${num_threads}"
        		mv ${benchmark}_omp.exe ${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-omp.exe
 
