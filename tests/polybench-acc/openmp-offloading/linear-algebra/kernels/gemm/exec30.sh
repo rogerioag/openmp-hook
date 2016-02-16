dcae8b62a62fc8f6230418f44414da7f468e7fc6
rogerio.rag <rogerio.rag@gmial.com>
2016-02-16 16:27:51 +0100

    Updating the script.
    Former-commit-id: f0d90089e217f89d8ab9d468979da35a081a51e2
    Former-commit-id: fa4b96b5950f18e9bd7f3b0d59eee9630b9e0aeb

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/exec30.sh
@@ -7,6 +7,8 @@ benchmark=`basename $PWD`
 # recupera o nome do diretorio, que é o nome do benchmark.
 echo "Executing $benchmark."
 
+mkdir output
+
 for size_of_data in LARGE_DATASET; do
 	for num_threads in 1 2 4 8 10 12; do
 		echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
@@ -17,7 +19,7 @@ for size_of_data in LARGE_DATASET; do
           		for ((  i = 1 ;  i <= 10;  i++  ))
 				do
             		echo "Execution ${i} of ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
-					./${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe >> data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.csv 2>> data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu-stderr.csv
+					./${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.exe >> output/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu.csv 2>> output/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-offloading-gpu-stderr.csv
 				done
 			done
 		done
