c39511bb32825f30422344faf1dbd4131c638ec7
rogerio.rag <rogerio.rag@gmial.com>
2016-02-18 13:10:57 +0100

    Testing jacobi2d code with others schedules.
    Former-commit-id: 6cacfb71d1ca7c0f9b8de3785e1d6ce9cda7a534

--- tests/polybench-acc/OpenMP/stencils/jacobi-2d-imper/jacobi-2d-imper.c
+++ tests/polybench-acc/OpenMP/stencils/jacobi-2d-imper/jacobi-2d-imper.c
@@ -149,7 +149,7 @@ void jacobi_2d_imper_omp_kernel(int tsteps,
 
   #pragma scop
   
-  #pragma omp master
+ /* #pragma omp master
   {
     for (t = 0; t < _PB_TSTEPS; t++) {
       #pragma omp parallel for private(i,j) schedule(OPENMP_SCHEDULE_WITH_CHUNK) num_threads(OPENMP_NUM_THREADS)
@@ -162,9 +162,27 @@ void jacobi_2d_imper_omp_kernel(int tsteps,
         for (j = 1; j < _PB_N-1; j++)
           A[i][j] = B[i][j];
     }
+  }*/
+  
+  #pragma omp parallel private(i,j,t)
+  {
+    #pragma omp master
+    {
+      for (t = 0; t < _PB_TSTEPS; t++)
+      {
+        #pragma omp for schedule(static) 
+        for (i = 1; i < _PB_N - 1; i++)
+          for (j = 1; j < _PB_N - 1; j++)
+            B[i][j] = 0.2 * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
+        #pragma omp for schedule(static) 
+        for (i = 1; i < _PB_N-1; i++)
+          for (j = 1; j < _PB_N-1; j++)
+            A[i][j] = B[i][j];
+      }
+    }
   }
+  #pragma endscop  
 
-  #pragma endscop
 }
 
 /* ------------------------------------------------------------- */
