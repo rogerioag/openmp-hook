d9a2bd827d1197a6495fa593340b4972d4f35356
rogerio.rag <rogerio.rag@gmial.com>
2016-02-29 15:57:40 +0100

    Updating the Makefile cuda.
    Former-commit-id: f17af3bb15137484413823e8f226939942424c8d

--- tests/polybench-acc/CUDA/utilities/common.mk
+++ tests/polybench-acc/CUDA/utilities/common.mk
@@ -1,6 +1,7 @@
 all:
-	nvcc ${OPT_LEVEL} ${CUFILES} -I${PATH_TO_UTILS} -o ${EXECUTABLE} -DPOLYBENCH_TIME -ccbin=${CXX}
+	${NVCC} ${OPT_LEVEL} -c ${CUFILES} -I${PATH_TO_UTILS} ${INCLUDE_DIR} ${POLYBENCH_OPTIONS} -ccbin=${CXX} -o ${EXECUTABLE}
 clean:
 	rm -f *~ *.exe
 
+ 
 
