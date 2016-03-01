c9367d54016cd9c93e8bcbeb5613cb60f4d5920a
rogerio.rag <rogerio.rag@gmial.com>
2016-03-01 17:20:25 +0100

    Updating jacobi-2d files.
    Former-commit-id: 6437e7053e3d82815f2e4e5835de8a4062c4cfb0

--- tests/polybench-acc/OpenMP/stencils/jacobi-2d-imper/jacobi-2d-imper.c
+++ tests/polybench-acc/OpenMP/stencils/jacobi-2d-imper/jacobi-2d-imper.c
@@ -150,18 +150,18 @@ void jacobi_2d_imper_omp_kernel(int tsteps,
   int tid;
 
   #pragma scop
-  #pragma omp parallel private(i,j,t) num_threads(OPENMP_NUM_THREADS)
+  // #pragma omp parallel private(i,j,t) num_threads(OPENMP_NUM_THREADS)
   {
-    #pragma omp master
+  //  #pragma omp master
     {
       for (t = 0; t < _PB_TSTEPS; t++)
       {
-        #pragma omp parallel for schedule(OPENMP_SCHEDULE_WITH_CHUNK) 
+//        #pragma omp parallel for schedule(OPENMP_SCHEDULE_WITH_CHUNK) 
         for (i = 1; i < _PB_N - 1; i++)
           for (j = 1; j < _PB_N - 1; j++)
             B[i][j] = 0.2 * (A[i][j] + A[i][j-1] + A[i][1+j] + A[1+i][j] + A[i-1][j]);
         
-        #pragma omp parallel for schedule(OPENMP_SCHEDULE_WITH_CHUNK) 
+//        #pragma omp parallel for schedule(OPENMP_SCHEDULE_WITH_CHUNK) 
         for (i = 1; i < _PB_N-1; i++)
           for (j = 1; j < _PB_N-1; j++)
             A[i][j] = B[i][j];
