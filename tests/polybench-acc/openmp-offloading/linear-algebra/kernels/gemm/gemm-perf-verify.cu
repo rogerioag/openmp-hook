01329b1ff94fd858e61f7b530c835d64fc7944d4
rogerio.rag <rogerio.rag@gmial.com>
2016-03-22 09:53:41 +0100

    Adding gemm perf test.
    Former-commit-id: a85728511053b0bd9e4334f15142be78ad4f8a35

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm-perf-verify.cu
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/gemm/gemm-perf-verify.cu
@@ -315,8 +315,6 @@ int main(int argc, char *argv[]) {
 
   fprintf(stdout, "exp, num_threads, NI, NJ, NK, ORIG, OMP\n");
 
-  fprintf(stdout, "OMP+OFF, %d, %d, %d, %d, ", OPENMP_NUM_THREADS, NI, NJ, NK);
-
   // fprintf(stderr, "calling gemm_original:\n");
   gemm_original(ni, nj, nk, alpha, beta, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C));
   fprintf(stdout, ", ");
