813b2c7ea930d98e1b69a91ef553a6ad0b164fe5
rogerio.rag <rogerio.rag@gmial.com>
2016-03-25 11:38:00 +0100

    Adding macro FORCENOOFFLOAD and DEV_OFFLOAD to control the FORCE NO OFFLOADING in the compilation.
    Former-commit-id: 5740e973839ae8569ff5055fdc2ec83e666cb88e

--- roofline.c
+++ roofline.c
@@ -1167,9 +1167,10 @@ int RM_get_better_device_to_execution(double oi){
 	fprintf(stderr, "oi_cpu, oi_gpu, ap_cpu, ap_gpu, best_ap, best_dev \n");
 	fprintf(stderr, "%10.10f, %10.10f, %10.10f, %10.10f, %d\n", oi, oi_gpu, RM_attainable_performance(0, oi), RM_attainable_performance(1, oi), best_ap, best_dev);
 	
-	// return 0;
-	
-	return best_dev;
+	/* Using this define for compilation parameter: FORCENOOFFLOAD to force no offloading to overhead verification. 
+	   In this case, return 0. Otherwise return best_dev.
+	*/
+	return DEV_OFFLOAD;
 }
 
 /* ------------------------------------------------------------ */
