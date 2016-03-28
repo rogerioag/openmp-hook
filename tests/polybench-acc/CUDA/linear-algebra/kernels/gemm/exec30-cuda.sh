068cf4cc2ea3d80c15c5e33321062cab06d4d2ea
rogerio.rag <rogerio.rag@gmial.com>
2016-03-28 18:38:20 +0200

    Updating CUDA gemm.
    Former-commit-id: a7c64bff465e1af29d30e46af4b3dd5d3a887f75
    Former-commit-id: 752ad94bc9a9944cc0247b5a87e4377c7fff1c9b

--- tests/polybench-acc/CUDA/linear-algebra/kernels/gemm/exec30-cuda.sh
+++ tests/polybench-acc/CUDA/linear-algebra/kernels/gemm/exec30-cuda.sh
@@ -4,10 +4,10 @@
 
 benchmark=`basename $PWD`
 
-experiment_date=`date +'%m-%d-%Y-%T'`
+experiment_date=`date +'%d-%m-%Y-%H-%M-%S'`
 OUTPUT=output/${experiment_date}
 
-echo "Executing test for $benchmark, start at $experiment_date"
+echo "Executing test for $benchmark, start at `date +'%d/%m/%Y-%T'`"
 
 mkdir -p ${OUTPUT}
 
@@ -17,9 +17,9 @@ for size_of_data in TOY_DATASET MINI_DATASET TINY_DATASET SMALL_DATASET MEDIUM_D
 	mv ${benchmark}-gpu.exe ${benchmark}-dataset-${size_of_data}-gpu.exe
     for ((  i = 1 ;  i <= 10;  i++  ))
 	do
-       	echo "Execution ${i} of ${benchmark} with dataset: ${size_of_data} start at `date +'%m-%d-%Y-%T'`"
+       	echo "Execution ${i} of ${benchmark} with dataset: ${size_of_data} start at `date +'%d/%m/%Y-%T'`"
        	echo "Execution = ${i}, benchmark = ${benchmark}, size_of_data = ${size_of_data}," >> ${OUTPUT}/data-${benchmark}-dataset-${size_of_data}-gpu.csv
 		./${benchmark}-dataset-${size_of_data}-gpu.exe >> ${OUTPUT}/data-${benchmark}-dataset-${size_of_data}-gpu.csv 2>> ${OUTPUT}/data-${benchmark}-dataset-${size_of_data}-gpu-stderr.csv
 	done
 done
-echo "End of tests at `date +'%m-%d-%Y-%T'`"
+echo "End of tests at `date +'%d/%m/%Y-%T'`"
