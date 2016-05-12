cc6c39e6cc474d6b64f70f2fc2833a204584aaa0
rogerio.rag <rogerio.rag@gmial.com>
2016-05-11 23:17:43 -0300

    Updating script exec 30 omp to test chunk size.
    Former-commit-id: 2e2dfa849e586d948a7c1015776c3c6750d77924

--- tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/exec30-omp.sh
+++ tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/exec30-omp.sh
@@ -12,7 +12,7 @@ echo "Executing test for $benchmark, start at `date +'%d/%m/%Y-%T'`"
 mkdir -p ${OUTPUT}
 
 for size_of_data in TOY_DATASET MINI_DATASET TINY_DATASET SMALL_DATASET MEDIUM_DATASET STANDARD_DATASET LARGE_DATASET EXTRALARGE_DATASET; do
-    for num_threads in 1 2 4 8 10 12 16 18 20 22 24; do
+    for num_threads in 12 24; do
         echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
         for omp_schedule in DYNAMIC; do
             for chunk_size in 32 64 128 256; do
