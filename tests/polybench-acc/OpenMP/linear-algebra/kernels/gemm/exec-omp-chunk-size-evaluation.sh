3e2cd4a703d4883b5d86b2219f208ddb7e81559a
rogerio.rag <rogerio.rag@gmial.com>
2016-06-06 10:51:03 -0300

    Updating chunk size evaluation script for omp version.
    Former-commit-id: 43c22f9c6460c983ca67fcd76581be8e79f25755

--- tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/exec-omp-chunk-size-evaluation.sh
+++ tests/polybench-acc/OpenMP/linear-algebra/kernels/gemm/exec-omp-chunk-size-evaluation.sh
@@ -19,10 +19,10 @@ mkdir -p ${OUTPUT}
 # for size_of_data in TOY_DATASET MINI_DATASET TINY_DATASET SMALL_DATASET MEDIUM_DATASET STANDARD_DATASET LARGE_DATASET; do
 	for size_of_data in LARGE_DATASET; do
 	#for num_threads in 1 2 4 8 10 12 16 18 20 22 24; do
-	for num_threads in 12 24; do
+	for num_threads in 24 22 20 18 16 14 12 10 8 6 4 2 1; do
 		echo "Compiling ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
 		for omp_schedule in DYNAMIC; do
-			for chunk_size in 32 64 128 256; do
+			for chunk_size in 16 32 64 128 256; do
 				make POLYBENCH_OPTIONS="-DPOLYBENCH_TIME -D${size_of_data}" OMP_CONFIG="-DOPENMP_SCHEDULE_${omp_schedule} -DOPENMP_CHUNK_SIZE=${chunk_size} -DOPENMP_NUM_THREADS=${num_threads}"
 				mv ${benchmark}-${PREFIX_BENCHMARK}.exe ${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${PREFIX_BENCHMARK}.exe
           			for ((  i = 1 ;  i <= 10;  i++  ))
