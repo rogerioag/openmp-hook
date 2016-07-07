1abdb833675e866b0b3707d31846664e94bdc3b7
rogerio.rag <rogerio.rag@gmial.com>
2016-07-07 10:18:40 -0300

    Sincronizing before of no offloading experiments.
    Former-commit-id: 6dccccafce5077fb812a218997aa73b5019dafa8

--- experiments/chunk_size_evaluation/join-omp-omp-off-cuda.sh
+++ experiments/chunk_size_evaluation/join-omp-omp-off-cuda.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <benchmark>" >&2
@@ -16,20 +16,26 @@ for experiment_date in `ls ${INPUT}`; do
     if [ -d "${INPUT}/${experiment_date}" ]
     then
         mkdir -p ${OUTPUT}
-        echo "" >> ${OUTPUT}/${experiment_date}-joined.csv
+        echo "" > ${OUTPUT}/${benchmark}-${experiment_date}-joined.csv
         for size_of_data in LARGE_DATASET; do
             for num_threads in 24 22 20 18 16 14 12 10 8 6 4 2 1; do
-                for omp_schedule in DYNAMIC; do
-                    for chunk_size in 16 32 64 128 256; do
+                for omp_schedule in DYNAMIC NONE; do
+                    for chunk_size in 0 16 32 64 128 256; do
                         echo "Processing ${i} of ${benchmark} with dataset: ${size_of_data}, schedule: ${omp_schedule}, chunk: ${chunk_size}, threads: ${num_threads}."
-                        for version in omp offloading-gpu; do
+                        for version in openmp openmp-offloading cuda; do
                             # ${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv
                             if [ -f "${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv" ]
                             then
-                                echo "File: ${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv"
-                                cat ${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv >> ${OUTPUT}/${experiment_date}-joined.csv
+                                # if o arquivo tem no conteudo o nome do benchmark.
+                                #if grep -q ${benchmark} "${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv";
+                                #then
+                                    echo "File: ${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv [OK]"
+                                    cat ${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv >> ${OUTPUT}/${benchmark}-${experiment_date}-joined.csv
+                                #else
+                                #    cat ${OUTPUT}/${benchmark}-${experiment_date}-joined.csv ${OUTPUT}/bosta.txt
+                                #fi
                             else
-                                echo "${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv not found."
+                                echo "${INPUT}/${experiment_date}/data-${benchmark}-dataset-${size_of_data}-schedule-${omp_schedule}-chunk-${chunk_size}-threads-${num_threads}-${version}.csv [NOT FOUND]"
                             fi
                         done
                     done
@@ -38,4 +44,4 @@ for experiment_date in `ls ${INPUT}`; do
         done
     fi
 done
-echo "End of tests at `date +'%d/%m/%Y-%T'`"
+echo "End of process at `date +'%d/%m/%Y-%T'`"
