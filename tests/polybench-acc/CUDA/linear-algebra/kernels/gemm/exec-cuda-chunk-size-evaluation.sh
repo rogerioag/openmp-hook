ecb32a06bee78821f63fe2f0038f12e68cb28801
rogerio.rag <rogerio.rag@gmial.com>
2016-08-08 20:44:27 -0300

    Adding chunk size evaluation experiment cuda version.
    Former-commit-id: c49b54dad9ee5b168c7cb1e658f660cba0f4211d

--- tests/polybench-acc/CUDA/linear-algebra/kernels/gemm/exec-cuda-chunk-size-evaluation.sh
+++ tests/polybench-acc/CUDA/linear-algebra/kernels/gemm/exec-cuda-chunk-size-evaluation.sh
@@ -21,7 +21,7 @@ echo "Executing test for $benchmark, start at `date +'%d/%m/%Y-%T'`"
 mkdir -p ${OUTPUT}
 
 # for size_of_data in TOY_DATASET MINI_DATASET TINY_DATASET SMALL_DATASET MEDIUM_DATASET STANDARD_DATASET LARGE_DATASET EXTRALARGE_DATASET; do
-for size_of_data in LARGE_DATASET; do
+for size_of_data in EXTRALARGE_DATASET; do
 	for num_threads in 1; do		
 		for omp_schedule in NONE; do
 			for chunk_size in 0; do
