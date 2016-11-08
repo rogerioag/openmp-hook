d4f34a4b0905911dce1241218650be941b2d4e59
Rogério Gonçalves <rogerio.rag@gmail.com>
2016-11-08 20:50:16 -0200

    Updating the sizes for tests.
    Former-commit-id: fb52faf2d40a06acba7c468a9c400192f2665b65

--- tests/polybench-acc/openmp-offloading/linear-algebra/kernels/vectoradd/vectoradd.h
+++ tests/polybench-acc/openmp-offloading/linear-algebra/kernels/vectoradd/vectoradd.h
@@ -3,71 +3,56 @@
 # define VECTORADD_H
 
 /* Default to STANDARD_DATASET. */
-#if !defined(TOY_DATASET) && !defined(MINI_DATASET) && !defined(TINY_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET) && !defined(HUGE_DATASET)
+#if !defined(TOY_DATASET) && !defined(MINI_DATASET) && !defined(TINY_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET) && !defined(HUGE_DATASET) && !defined(BIG_HUGE_DATASET)
 #define STANDARD_DATASET
 #endif
 
 /* Do not define anything if the user manually defines the size. */
-# if !defined(NI) && !defined(NJ) && !defined(NK)
+# if !defined(NI)
 /* Define the possible dataset sizes. */
 #ifdef TOY_DATASET
 #define NI 32
-#define NJ 32
-#define NK 32
 #endif
 
 #ifdef MINI_DATASET
 #define NI 64
-#define NJ 64
-#define NK 64
 #endif
 
 #ifdef TINY_DATASET
 #define NI 128
-#define NJ 128
-#define NK 128
 #endif
 
 #ifdef SMALL_DATASET
 #define NI 256
-#define NJ 256
-#define NK 256
 #endif
 
 #ifdef MEDIUM_DATASET
 #define NI 512
-#define NJ 512
-#define NK 512
 #endif
 
 #ifdef STANDARD_DATASET /* Default if unspecified. */
 #define NI 1024
-#define NJ 1024
-#define NK 1024
 #endif
 
 #ifdef LARGE_DATASET
 #define NI 2048
-#define NJ 2048
-#define NK 2048
 #endif
 
 #ifdef EXTRALARGE_DATASET
 #define NI 4096
-#define NJ 4096
-#define NK 4096
 #endif
 
 #ifdef HUGE_DATASET
 #define NI 8192
-#define NJ 8192
-#define NK 8192
 #endif
+
+#ifdef BIG_HUGE_DATASET
+#define NI 1000000
+#endif
+
 #endif /* !N */
 
 #define _PB_NI POLYBENCH_LOOP_BOUND(NI,ni)
-#define _PB_NJ POLYBENCH_LOOP_BOUND(NJ,nj)
-#define _PB_NK POLYBENCH_LOOP_BOUND(NK,nk)
 
 #ifndef DATA_TYPE
 #define DATA_TYPE double
